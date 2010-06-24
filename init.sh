485e9f2a571b3e5a52772d9a4e012f6fa68dc6ef
Kridsada Thanabulpong <sirn@ogsite.net>
2010-06-23 21:29:37 -0700

    Reorganizing stuffs.

--- init.sh
+++ init.sh
@@ -9,9 +9,25 @@ function _link_to_home {
         echo "* Skipping $2 as it is already exist."
     else
         echo "* Linking $2 to the home directory"
+        mkdir -p `dirname $dest_path` 2>/dev/null
         ln -s $source_path $dest_path
     fi
 }
 
 # Installation
 _link_to_home zsh/zshrc .zshrc
+_link_to_home elisp/emacs .emacs
+_link_to_home vim/vimrc .vimrc
+_link_to_home vim/vimperatorrc .vimperatorrc
+_link_to_home etc/gitconfig .gitconfig
+_link_to_home etc/screenrc .screenrc
+_link_to_home etc/ssh_config .ssh/config
+
+if [[ $1 == "--with-x" ]]; then
+    _link_to_home etc/ratpoisonrc .ratpoisonrc
+    _link_to_home etc/Xdefaults .Xdefaults
+fi
+
+if [[ -e ~/.config/awesome ]]; then
+    _link_to_home etc/awesome.lua ~/.config/awesome/rc.lua
+fi
