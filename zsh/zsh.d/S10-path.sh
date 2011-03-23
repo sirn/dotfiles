8c4e7b623aed40dc78b0328eec5b3b712364f74e
Kridsada Thanabulpong <sirn@ogsite.net>
2011-03-24 04:55:06 +0700

    Separate JavaScript dotfiles.

--- zsh/zsh.d/S10-path.sh
+++ zsh/zsh.d/S10-path.sh
@@ -1,5 +1,5 @@
 # Check if the provided directory exist and append $PATH if they do.
-_add_to_exec() {
+function _add_to_exec {
     if [[ -e $1 ]]; then
         export PATH="$1/bin:$PATH"
         if [ -e "$1/sbin" ]; then
@@ -7,6 +7,17 @@ _add_to_exec() {
         fi
     fi
 }
+
+function _add_brew_path {}
+if [[ -n `brew 2>/dev/null` ]]; then
+    function _add_brew_path {
+      brew --prefix $1 | while read prefix; do
+          if [[ -e $prefix ]]; then
+              _add_to_exec $prefix$2
+          fi
+      done
+    }
+fi
 
 # Reset original path
 export PATH="/bin:/sbin"
@@ -23,6 +34,5 @@ _add_to_exec ~/.gem/ruby/1.8
 
 # Application-specific
 _add_to_exec /Library/PostgreSQL/8.4
-_add_to_exec /usr/local/share/npm
 
 # vim:ft=zsh
---
+++ zsh/zsh.d/S40-javascript.sh
@@ -0,0 +1,5 @@
+# NodeJS's npm
+_add_to_exec /usr/local/share/npm
+
+# Narwhal's tusk
+_add_brew_path narwhal /libexec
