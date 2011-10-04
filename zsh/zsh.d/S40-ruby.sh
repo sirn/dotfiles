ce9b67b6ee083a6012f78020127968b12378332f
Kridsada Thanabulpong <sirn@ogsite.net>
2011-10-04 10:19:29 +0700

    Add Rbenv support

--- zsh/zsh.d/S40-ruby.sh
+++ zsh/zsh.d/S40-ruby.sh
@@ -3,3 +3,9 @@ local rvm_script_path="$HOME/.rvm/scripts/rvm"
 if [[ -e $rvm_script_path ]]; then
   source $rvm_script_path
 fi
+
+# Rbenv
+_add_to_exec $HOME/.rbenv
+if [[ -e $HOME/.rbenv ]]; then
+  eval "$(rbenv init -)"
+fi
