081089145d339ed18c6c88d721f994132029836d
Kridsada Thanabulpong <sirn@ogsite.net>
2012-01-03 18:06:34 +0700

    Remove RVM.

--- zsh/zsh.d/S40-ruby.sh
+++ zsh/zsh.d/S40-ruby.sh
@@ -1,10 +1,4 @@
 # RVM
-local rvm_script_path="$HOME/.rvm/scripts/rvm"
-if [[ -e $rvm_script_path ]]; then
-  source $rvm_script_path
-fi
-
-# Rbenv
 _add_to_exec $HOME/.rbenv
 if [[ -e $HOME/.rbenv ]]; then
   eval "$(rbenv init -)"
