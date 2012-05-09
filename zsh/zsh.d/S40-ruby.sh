c3f9ad18a08021b27db556cf2df7f7380dc4f91f
Kridsada Thanabulpong <sirn@ogsite.net>
2012-05-09 08:11:57 +0700

    Derp.

--- zsh/zsh.d/S40-ruby.sh
+++ zsh/zsh.d/S40-ruby.sh
@@ -3,3 +3,8 @@ _add_to_exec $HOME/.rbenv
 if [[ -e $HOME/.rbenv ]]; then
   eval "$(rbenv init -)"
 fi
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+  source $HOME/.rvm/scripts/rvm
+  export PATH=$PATH:$HOME/.rvm/bin
+fi
