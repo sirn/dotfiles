ee62504682a4698f97af1ff053487f2e4921e214
Kridsada Thanabulpong <sirn@ogsite.net>
2012-06-10 14:51:06 +0700

    Remove unnecessary Python setup from ZSH.

--- zsh/zsh.d/S49-python.sh
+++ zsh/zsh.d/S49-python.sh
@@ -6,36 +6,18 @@
 # | OSX       | /System/Library/Frameworks/Python.framework/Versions/0.0
 # | Linux     | /usr/
 # +-----------+----------------------------------------------------------
-# Only setup Homebrew for the time being.
-_add_python() {
-  if [[ -e $1 ]]; then
-    export PATH=$1/bin:$PATH
-    if [[ -z $PYTHON_PATH ]]; then
-      export PYTHON_PATH=$1/bin
-    fi
-  fi
-}
 
+# Default path
+export VIRTUALENV_PATH=/usr/local/bin
+
+# Homebrew
 if [[ -n `brew 2>/dev/null` ]]; then
-  brew --prefix python python3 pypy | while read prefix; do
-    _add_python $prefix
-  done
-fi
-
-# Linux
-if [[ -n $IS_LINUX ]]; then
-  _add_python /usr
+  export VIRTUALENV_PATH=/usr/local/share/python
+  export PATH=/usr/local/share/python:$PATH
 fi
 
 # VirtualEnv
-export VIRTUALENV_PATH=$PYTHON_PATH
-brew_shared=/usr/local/share/python
-if [[ -e $brew_shared ]]; then
-  export VIRTUALENV_PATH=$brew_shared
-  export PATH=$brew_shared:$PATH
-fi
-if [[ -e $VIRTUALENV_PATH/virtualenvwrapper_lazy.sh ]]; then
-    # Avoid loading VirtualEnvWrapper twice, since it is extremely slow
+if [[ -e $VIRTUALENV_PATH/virtualenvwrapper.sh ]]; then
     if [[ -z $VIRTUALENVWRAPPER_PYTHON ]]; then
         export WORKON_HOME=$HOME/.virtualenvs
         export VIRTUAL_ENV_DISABLE_PROMPT=yes
