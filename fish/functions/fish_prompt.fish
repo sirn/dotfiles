9cfaed6569f8f36ca3fbddb7a7d8353b6bdc7a24
Kridsada Thanabulpong <sirn@ogsite.net>
2013-07-19 02:50:35 +0700

    Better fish prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,35 +1,42 @@
 function fish_prompt --description 'Write out the prompt'
 
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-  end
-
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
-
-  switch $USER
-
-    case root
-
-    if not set -q __fish_prompt_cwd
-      if set -q fish_color_cwd_root
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-      else
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-      end
+    # Calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
 
-    echo -n -s "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '# '
+    # Color for host section.
+    if not set -q __fish_prompt_host
+        set -g __fish_prompt_host (set_color -b 444444 ffffff)
+    end
+
+    # Reset color.
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    # Main switch, display prompt using different color for root.
+    switch $USER
+    case root
+
+        if not set -q __fish_prompt_cwd
+            if set -q fish_color_cwd_root
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            else
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+        end
 
     case '*'
 
-    if not set -q __fish_prompt_cwd
-      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        if not set -q __fish_prompt_cwd
+            set -g __fish_prompt_cwd (set_color -b $fish_color_cwd ffffff)
+        end
+
     end
 
-    echo -n -s "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '> '
-
-  end
+    echo -n -s \
+        "$__fish_prompt_host" " $__fish_prompt_hostname " \
+        "$__fish_prompt_normal" "$__fish_prompt_cwd " (prompt_pwd) " " \
+        "$__fish_prompt_normal" " "
 end
