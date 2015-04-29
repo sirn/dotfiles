2c86a0115b38f85ee745d4ea911a9924d768ab26
Kridsada Thanabulpong <sirn@ogsite.net>
2015-04-30 03:10:56 +0700

    Remove unused lines in Fish.

--- fish/config.fish
+++ fish/config.fish
@@ -3,56 +3,48 @@ if status --is-login
     # Fish reassigns $fish_greeting if it is not set so we can't do -e.
     set fish_greeting ""
 
-    # General bin paths
+    # General bin paths.
     set PATH /usr/local/sbin $PATH
     set PATH /usr/local/bin $PATH
     set PATH $HOME/.local/bin $PATH
     set PATH $HOME/.dotfiles/bin $PATH
-    set PATH $HOME/Dropbox/Library/Misc/bin $PATH
 
-    # Use VIM as editor if available
+    # Use VIM as editor if available.
     if which vim 2>&1 >/dev/null
         set EDITOR vim
         function vi; vim $argv; end
     end
 
-
-    # Ruby-specific configurations
+    # Ruby-specific configurations.
     if test -d $HOME/.rbenv
         set PATH $HOME/.rbenv/bin $PATH
         set PATH $HOME/.rbenv/shims $PATH
         rbenv rehash >/dev/null
     end
 
-    # Python-specific configurations
+    # Python-specific configurations.
     if test -d $HOME/.pyenv
         set PATH $HOME/.pyenv/bin $PATH
         set PATH $HOME/.pyenv/shims $PATH
         pyenv rehash >/dev/null
     end
 
-    # Node-specific configurations
+    # Node-specific configurations.
     if test -d /usr/local/share/npm/
         set PATH /usr/local/share/npm/bin $PATH
     end
 
-    # Directory bin
+    # Directory bin.
     set PATH ./bin $PATH
 
-    # EC2-specific paths
-    if test -d $HOME/.ec2
-        set EC2_PRIVATE_KEY (/bin/ls $HOME/.ec2/pk-*.pem)
-        set EC2_CERT (/bin/ls $HOME/.ec2/cert-*.pem)
-        set EC2_KEYPAIR (/bin/ls $HOME/.ec2/ec2-*.pem)
-    end
-
+    # Headless VMWare.
     if test -d /Applications/VMware\ Fusion.app
         function vmrun
             /Applications/VMware\ Fusion.app/Contents/Library/vmrun $argv
         end
     end
 
-    # Aliases
+    # Aliases.
     function intellij; open -b com.jetbrains.intellij $argv; end
     if which hub 2>&1 >/dev/null
         function git; hub $argv; end
