c527be7140a04e491a73356a2f996bc0f4c30127
Kridsada Thanabulpong <sirn@ogsite.net>
2016-02-17 16:59:06 +0700

    Add configurations added by custom.

--- emacs/spacemacs.d/init.el
+++ emacs/spacemacs.d/init.el
@@ -107,3 +107,20 @@
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((web-mode-code-indent-offset . 2)
+     (web-mode-css-indent-offset . 2)
+     (web-mode-markup-indent-offset . 2)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
