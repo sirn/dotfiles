b2cd9dbce27430afb243f604f947db1c0f93db21
Kridsada Thanabulpong <sirn@ogsite.net>
2016-09-16 23:22:59 +0700

    Refactor erlang-rebar to always use relative path.

--- emacs/spacemacs.d/layers/erlang-rebar/local/erlang-rebar/erlang-rebar.el
+++ emacs/spacemacs.d/layers/erlang-rebar/local/erlang-rebar/erlang-rebar.el
@@ -1,21 +1,25 @@
 (defun erlang-rebar-locate-root ()
   (locate-dominating-file default-directory "rebar.config"))
 
+;; TODO: Unfortunately broke if path contains space as rebar do not
+;; escape the path and we do not handle path escaping either.
 (defun erlang-rebar-code-path ()
-  (let ((default-directory (erlang-rebar-locate-root)))
-    (when default-directory
-      ;; TODO: Unfortunately broke if path contains space as rebar do not
-      ;; escape the path and we do not handle path escaping either.
-      (split-string (shell-command-to-string "rebar3 path")))))
+  (split-string (shell-command-to-string "rebar3 path")))
+
+(defun erlang-rebar-include-path ()
+  (split-string (shell-command-to-string "find . -iname include -type d")))
 
 (defun erlang-rebar-hook ()
-  (setq flycheck-erlang-include-path
-        (list "../include/" "../../include/" "../../"))
-  (let ((paths (erlang-rebar-code-path)))
-    (progn
-      (setq flycheck-erlang-library-path paths)
-      (add-to-list 'inferior-erlang-machine-options "-pa" t)
-      (dolist (path paths)
-        (add-to-list 'inferior-erlang-machine-options path t)))))
+  (let ((default-directory (erlang-rebar-locate-root)))
+    (when default-directory
+      (progn
+        (let ((paths (erlang-rebar-code-path)))
+          (progn
+            (setq flycheck-erlang-library-path paths)
+            (add-to-list 'inferior-erlang-machine-options "-pa" t)
+            (dolist (path paths)
+              (add-to-list 'inferior-erlang-machine-options path t))))
+        (dolist (path (erlang-rebar-include-path))
+          (add-to-list 'flycheck-erlang-include-path path t))))))
 
 (provide 'erlang-rebar)
