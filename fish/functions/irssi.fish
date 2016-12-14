35916e3839d17d51759d5abc1c30148a4aa3e273
Kridsada Thanabulpong <sirn@ogsite.net>
2016-12-15 05:25:47 +0700

    Slight utility cleanups.

--- fish/functions/irssi.fish
+++ fish/functions/irssi.fish
@@ -1,7 +1,8 @@
 function irssi
-    if which tmux 2>&1 >/dev/null
-        command tmux new -A -s irssi command irssi $argv \; set status off
+    set irssi_bin (type -P irssi)
+    if type -P tmux >/dev/null 2>&1
+      tmux new -A -s irssi $irssi_bin $argv \; set status off
     else
-        command irssi $argv
+      eval $irssi_bin $argv
     end
 end
--- fish/functions/vi.fish
+++ fish/functions/vi.fish
@@ -1,7 +1,7 @@
 function vi
-    if which nvim 2>&1 >/dev/null
+    if type -P nvim >/dev/null 2>&1
         command nvim $argv
-    else if which vim 2>&1 >/dev/null
+    else if type -P vim >/dev/null 2>&1
         command vim $argv
     else
         command vi $argv
