5fbf6013d513379101776b9f44cd6c4e312570d1
Kridsada Thanabulpong <sirn@ogsite.net>
2017-02-07 01:31:25 +0700

    Use source in fish config.
    Workaround fish shell 3809.

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,6 @@ if status --is-interactive
     set -x SHELL $__fish_bin_dir/fish
 
     if type -P direnv >/dev/null 2>&1
-       eval (direnv hook fish)
+        direnv hook fish | source
     end
 end
