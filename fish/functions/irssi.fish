88e4d100ca0a1c9f62e3d7e051c3ca4547a5423c
Kridsada Thanabulpong <sirn@ogsite.net>
2017-02-08 03:06:27 +0700

    Remove tmux-irssi wrapper.

--- fish/functions/irssi.fish
+++
@@ -1,8 +0,0 @@
-function irssi
-    set irssi_bin (type -P irssi)
-    if type -P tmux >/dev/null 2>&1
-      tmux new -A -s irssi $irssi_bin $argv \; set status off
-    else
-      eval $irssi_bin $argv
-    end
-end
