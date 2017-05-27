43a2cb5c00788d4566c5d4fccf059c1fbdc037b2
Kridsada Thanabulpong <sirn@ogsite.net>
2017-05-27 15:20:56 +0700

    Properly call $argv in Emacs alias.

--- fish/functions/emacs.fish
+++ fish/functions/emacs.fish
@@ -1,6 +1,6 @@
 function emacs
     if test -d /Applications/Emacs.app/
-        command /Applications/Emacs.app/Contents/MacOS/Emacs -nw
+        command /Applications/Emacs.app/Contents/MacOS/Emacs -nw $argv
     else
         command emacs -nw $argv
     end
