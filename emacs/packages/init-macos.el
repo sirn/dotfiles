d71fa11bcb1db5fff24dfc0e452cf3f552934a74
Kridsada Thanabulpong <sirn@ogsite.net>
2017-06-17 14:59:05 +0700

    Few customization for Emacs on macOS.

---
+++ emacs/packages/init-macos.el
@@ -0,0 +1,27 @@
+(when (eq system-type 'darwin)
+
+  (when (display-graphic-p)
+
+    (defun mac-toggle-fullscreen ()
+      (interactive)
+      (when (eq window-system 'mac)
+	(set-frame-parameter
+	 nil 'fullscreen
+	 (when (not (frame-parameter nil 'fullscreen)) 'fullscreen))))
+
+    (setq mac-command-key-is-meta nil)
+    (setq mac-command-modifier 'super)
+    (setq mac-option-key-is-meta t)
+    (setq mac-option-modifier 'meta)
+
+    (global-set-key (kbd "s-v") 'yank)
+    (global-set-key (kbd "s-c") 'evil-yank)
+    (global-set-key (kbd "s-w") 'delete-window)
+    (global-set-key (kbd "s-W") 'delete-frame)
+    (global-set-key (kbd "s-n") 'make-frame))
+
+  (req-package osx-trash
+    :init (osx-trash-setup))
+
+  (req-package pbcopy
+    :init (turn-on-pbcopy)))
