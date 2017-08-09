a763dc23290ff51223c2e336ae7124a9ca100053
Kridsada Thanabulpong <sirn@ogsite.net>
2017-08-09 12:44:02 +0700

    Silence exec-path-from-shell-check-startup-files with reasons.

--- emacs/packages/init-shell.el
+++ emacs/packages/init-shell.el
@@ -1,5 +1,11 @@
 (req-package exec-path-from-shell
   :config
+  ;; Disable environment check during startup because .rbenv/.pyenv need to be
+  ;; initialized as part of the interactive shell, otherwise anything that were
+  ;; installed as part of the distro package and contains shebangs referencing
+  ;; #!/usr/bin/env will be broken because python/ruby is now the rbenv/pyenv's
+  ;; which may not the contain necessary modules required by the distro package.
+  (setq exec-path-from-shell-check-startup-files nil)
   (exec-path-from-shell-initialize))
 
 (req-package keychain-environment
