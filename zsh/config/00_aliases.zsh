1fb6a81aa0b3daf067fd4ea68963801c9310479d
Kridsada Thanabulpong <sirn@ogsite.net>
2018-01-16 06:27:50 +0700

    Use GHQ to manage project paths.

--- zsh/config/00_aliases.zsh
+++ zsh/config/00_aliases.zsh
@@ -2,6 +2,11 @@ alias ls="ls -G"
 alias ll="ls -alh"
 alias emacs="emacs -nw"
 
+if hash fzy 2>/dev/null; then
+    alias ff="find . -type f |fzy"
+    alias fd="find . -type d |fzy"
+fi
+
 if hash nvim 2>/dev/null; then
     alias vim="nvim"
 fi
