49c999960fcd9174b6d9afc53d32b8ffaa5107c9
Kridsada Thanabulpong <sirn@ogsite.net>
2018-01-16 05:19:21 +0700

    Use hub command line.

--- zsh/config/00_aliases.zsh
+++ zsh/config/00_aliases.zsh
@@ -1,5 +1,11 @@
 alias ls="ls -G"
 alias ll="ls -alh"
-alias vi="nvim"
-alias vim="nvim"
 alias emacs="emacs -nw"
+
+if hash nvim 2>/dev/null; then
+    alias vim="nvim"
+fi
+
+if hash hub 2>/dev/null; then
+    alias git="hub"
+fi
