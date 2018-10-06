f03aa30fce8e40c435fa669043571691f9ce0434
Kridsada Thanabulpong <sirn@ogsite.net>
2018-10-06 19:49:57 +0800

    Configure pf in workspace.

---
+++ _provision/roles/services/files/pf/devd_anchor.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# A devd hook to automatically setup pf firewall for dynamically created
+# network interfaces such as vboxnet or zerotier. To use this script
+# create /usr/local/etc/devd/pf_ifnet.conf with:
+#
+#     notify 10 {
+#         match "system"        "IFNET";
+#         match "subsystem"     "vboxnet[0-9]+";
+#         match "type"          "ATTACH";
+#         action "/usr/local/libexec/local-pf-ifinet $subsystem up";
+#     }
+#
+#     notify 10 {
+#         match "system"        "IFNET";
+#         match "subsystem"     "vboxnet[0-9]+";
+#         match "type"          "DETACH";
+#         action "/usr/local/libexec/local-pf-ifinet $subsystem down";
+#     }
+#
+# Then configure pf.conf with the following line:
+#
+#     anchor "pfifnet/*"
+#
+# Then put this script on the path given in action. Note that on some
+# tap interface such as zerotier, you may want to monitor LINK_UP
+# and LINK_DOWN instead.
+#
+
+_tag="pf_ifnet"
+_prefix="pfifnet"
+
+hook_up() {
+    echo "pass in quick on $1 all" | pfctl -a "$_prefix/$1" -f -
+    logger -t "$_tag" "Added pf anchor for $1."
+}
+
+hook_down() {
+    pfctl -a "$_prefix/$1" -F rules
+    logger -t "$_tag" "Removed pf anchor for $1."
+}
+
+case "$2" in
+    up)   hook_up   "$1";;
+    down) hook_down "$1";;
+    *)
+        logger -t "$_tag" "Oink. I don't know what to do with $2 on $1."
+        exit 1
+        ;;
+esac
