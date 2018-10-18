e23520d7fcf4d63e1ef8fe5a3ee74481cb292d28
Kridsada Thanabulpong <sirn@ogsite.net>
2018-10-18 23:24:34 +0700

    Make gpg-agent great again (in ssh-agent mode)

--- emacs/packages/editor-auth.el
+++ emacs/packages/editor-auth.el
@@ -28,7 +28,30 @@
 
 
 (use-package pinentry
-  :straight t)
+  :straight t
+
+  :config
+
+  ;; Allow gpg-connect-agent in ssh-agent mode to forward pinentry to Emacs
+  ;; since the ssh-agent protocol has no way to pass the TTY to gpg-agent.
+  ;; See also --enable-ssh-support in gpg-agent(1)
+  ;;
+  ;; gpg-agent use INSIDE_EMACS environment variable to detect that we're
+  ;; running in Emacs, but the environment variable is set in term-mode.
+  ;; We do it here so all pinentry actually goes to Emacs.
+  ;;
+  ;; Also this hook has a nice effect of auto-starting gpg-agent when
+  ;; needed by ssh.
+  (setenv "INSIDE_EMACS" emacs-version)
+
+  (defun gr/gpg-update-tty (&rest _args)
+    (shell-command
+     "gpg-connect-agent updatestartuptty /bye"
+     " *gpg-update-tty*"))
+
+  (with-eval-after-load 'magit
+    (advice-add 'magit-start-git :before 'gr/gpg-update-tty)
+    (advice-add 'magit-call-git :before 'gr/gpg-update-tty)))
 
 
 (use-package epa
---
+++ zsh/config/99_gpg.zsh
@@ -0,0 +1,18 @@
+# This preexec hook make sure gpg-agent startuptty is set to the current
+# tty before running anything that may invoke ssh-agent.
+#
+# This is only required when gpg-agent is being used as ssh-agent, as
+# ssh-agent protocol has no way to pass the correct TTY to gpg-agent.
+# See also --enable-ssh-support in gpg-agent(1)
+
+_gpg_update_tty_hook () {
+    case "$1" in
+        *ssh* | *sftp* | *scp* | *git* | *rsync* | *mosh* )
+            gpg-connect-agent updatestartuptty /bye >/dev/null
+            ;;
+        * )
+            ;;
+    esac
+}
+
+add-zsh-hook preexec _gpg_update_tty_hook
