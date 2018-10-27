15745c26b65a6fbf0570f8fa0cfa52a42076f067
Kridsada Thanabulpong <sirn@ogsite.net>
2018-10-28 03:14:49 +0700

    Install stuff that are relevant to ZSH via zplug.

--- zsh/config/50_fzf.zsh
+++ zsh/config/50_fzf.zsh
@@ -1,9 +1,4 @@
-case "$(uname)" in
-    FreeBSD) FZF_PATH="/usr/local/share/examples/fzf/shell";;
-    Darwin)  FZF_PATH="/usr/local/opt/fzf/shell";;
-esac
-
-if [ -d "$FZF_PATH" ]; then
-    source "$FZF_PATH/key-bindings.zsh"
-    source "$FZF_PATH/completion.zsh"
+if [ -d "$ZPLUG_REPOS/junegunn/fzf" ]; then
+    source "$ZPLUG_REPOS/junegunn/fzf/shell/key-bindings.zsh"
+    source "$ZPLUG_REPOS/junegunn/fzf/shell/completion.zsh"
 fi
