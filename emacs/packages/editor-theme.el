9408f8916f510ccfb12e03e5cf85d4f34efccda6
Kridsada Thanabulpong <sirn@ogsite.net>
2018-11-05 05:45:48 +0700

    Colors and LibreSSL.

--- emacs/packages/editor-theme.el
+++ emacs/packages/editor-theme.el
@@ -5,5 +5,22 @@
 
 (use-package doom-themes
   :straight t
+
   :config
   (load-theme 'doom-city-lights t))
+
+
+(use-package solaire-mode
+  :straight t
+
+  :preface
+  (eval-when-compile
+    (declare-function turn-on-solaire-mode nil)
+    (declare-function solaire-mode-in-minibuffer nil)
+    (declare-function solaire-mode-swap-bg nil))
+
+  :config
+  (add-hook 'after-revert-hook 'turn-on-solaire-mode)
+  (add-hook 'change-major-mode-hook 'turn-on-solaire-mode)
+  (add-hook 'ediff-prepare-buffer-hook 'turn-on-solaire-mode)
+  (add-hook 'minibuffer-setup-hook 'solaire-mode-in-minibuffer))
--- zsh/config/99_terminfo.zsh
+++ zsh/config/99_terminfo.zsh
@@ -1,4 +1,9 @@
-if [ -f "$HOME/.terminfo/78/xterm-24bits" ] && [ -z "$TMUX" ]; then
-    TERMINFO="$HOME/.terminfo"
-    TERM=xterm-24bits
-fi
+case "$TERM" in
+    screen*) ;;
+    *)
+        TERM="xterm-256color"
+        if infocmp xterm-24bits >/dev/null 2>&1; then
+            TERM="xterm-24bits"
+        fi
+        ;;
+esac
