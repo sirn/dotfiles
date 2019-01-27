f34c492ed2ab492426da0cd99186b6c208379813
Kridsada Thanabulpong <sirn@ogsite.net>
2019-01-27 09:40:29 +0700

    Fix control persist.

--- emacs/packages/editor-tramp.el
+++ emacs/packages/editor-tramp.el
@@ -1,5 +1,6 @@
 (use-package tramp
   :config
   (put 'temporary-file-directory 'standard-value '("/tmp"))
+  (setq tramp-use-ssh-controlmaster-options nil)
   (setq tramp-auto-save-directory "~/.cache/emacs/backups")
   (setq tramp-persistency-file-name "~/.emacs.d/data/tramp"))
