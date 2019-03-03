f8adbd649fc5ee181bced61590d0726e25e03105
Kridsada Thanabulpong <sirn@ogsite.net>
2019-03-03 23:16:41 +0700

    Use remote PATH when running in TRAMP.

--- emacs/packages/editor-tramp.el
+++ emacs/packages/editor-tramp.el
@@ -1,6 +1,7 @@
 (use-package tramp
   :config
   (put 'temporary-file-directory 'standard-value '("/tmp"))
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
   (setq tramp-use-ssh-controlmaster-options nil)
   (setq tramp-auto-save-directory "~/.cache/emacs/backups")
   (setq tramp-persistency-file-name "~/.emacs.d/data/tramp"))
