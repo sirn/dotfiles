4134dfbd8920d7bca854a9d73274d6fc23da321c
Kridsada Thanabulpong <sirn@ogsite.net>
2019-03-22 23:19:05 +0700

    Migrating workspace: remove zfstools

--- _provision/roles/services/files/zfstools/periodic_daily.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -f /usr/local/sbin/zfs-auto-snapshot ]; then
-    /usr/local/sbin/zfs-auto-snapshot daily 7
-fi
--- _provision/roles/services/files/zfstools/periodic_weekly.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -f /usr/local/sbin/zfs-auto-snapshot ]; then
-    /usr/local/sbin/zfs-auto-snapshot weekly 4
-fi
