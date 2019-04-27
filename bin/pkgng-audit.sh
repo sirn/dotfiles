b7c5bf92041111908c9277e8ffef66fdd1f381a5
Kridsada Thanabulpong <sirn@ogsite.net>
2019-04-27 16:06:46 +0700

    Remove scripts that are no longer used
    * freebsd-genboot -- I have since switched to a simpler approach of
      using remote serial console to log into the machine during boot to
      enter the GELI password
    
    * openbsd-fetch -- There's now sysupgrade in base which uses the new
      bsd.upgrade to perform unattended upgrade
    
    * pkgng-audit -- Never really used

--- bin/pkgng-audit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# This is a script for auditing FreeBSD packages that are marked as
-# manually installed.
-#
-
-for pkg in $(pkg query -e '%a = 0' %o); do
-    sudo pkg set -A 1 "$pkg"
-done
