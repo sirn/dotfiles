870ee086349cc0b1128401fe9d7cebe522985b65
Kridsada Thanabulpong <sirn@ogsite.net>
2019-05-20 12:03:32 +0700

    Use fluid width for LUI in Circe

--- etc/emacs/packages/ext-irc.el
+++ etc/emacs/packages/ext-irc.el
@@ -29,4 +29,18 @@
   (require 'circe-color-nicks)
   (require 'circe-new-day-notifier)
   (enable-circe-color-nicks)
-  (enable-circe-new-day-notifier))
+  (enable-circe-new-day-notifier)
+
+  (setq lui-time-stamp-format "%H:%M")
+  (setq lui-time-stamp-position 'right-margin)
+  (setq lui-fill-type nil)
+
+  (defun gr/lui-setup ()
+    (setq fringes-outside-margins t)
+    (setq right-margin-width 5)
+    (setq word-wrap t)
+    (setq wrap-prefix "    ")
+    (if (display-graphic-p)
+      (setf (cdr (assoc 'continuation fringe-indicator-alist)) nil)))
+
+  (add-hook 'lui-mode-hook 'gr/lui-setup))
