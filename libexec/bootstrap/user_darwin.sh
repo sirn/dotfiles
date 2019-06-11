7596f8d17149741627405e0c974a9c75aff0617a
Kridsada Thanabulpong <sirn@ogsite.net>
2019-06-11 22:39:05 +0700

    Restore IPFS configurations in Darwin user bootstrap

--- libexec/bootstrap/user_darwin.sh
+++ libexec/bootstrap/user_darwin.sh
@@ -13,12 +13,37 @@ ensure_paths required same_root
 ensure_platform "Darwin"
 
 FLAVORS=$*
+IPFS_PLIST=$HOME/Library/LaunchAgents/io.ipfs.ipfs.plist
+
+
+## Setup
+##
+
+_setup_ipfs() {
+    printe_h2 "Setting up ipfs..."
+
+    if ! command -v ipfs >/dev/null; then
+        printe_info "ipfs is not installed, skipping..."
+        return
+    fi
+
+    if is_force || [ ! -f "$IPFS_PLIST" ]; then
+        cp "$BOOTSTRAP_ROOT/share/examples/launchd/io.ipfs.ipfs.plist" "$IPFS_PLIST"
+        chmod 0644 "$IPFS_PLIST"
+        launchctl load -w "$IPFS_PLIST"
+        printe "$IPFS_PLIST has been installed, you may need to relogin"
+    else
+        printe "$IPFS_PLIST already exists"
+    fi
+}
 
 
 ## Run
 ##
 
 _run() {
+    _setup_ipfs
+
     "$BOOTSTRAP_ROOT/libexec/bootstrap/user_shell.sh" "$FLAVORS"
     "$BOOTSTRAP_ROOT/libexec/bootstrap/user_links.sh" "$FLAVORS"
 }
