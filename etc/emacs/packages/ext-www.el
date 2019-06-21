e1e3a34d73793fd5dbd2c8d685fd35c720ca9aa5
Kridsada Thanabulpong <sirn@ogsite.net>
2019-06-21 16:36:48 +0000

    Fix w3m default search engine not properly formatting DuckDuckGo

--- etc/emacs/packages/ext-www.el
+++ etc/emacs/packages/ext-www.el
@@ -4,9 +4,14 @@
 
   :preface
   (eval-when-compile
+    (defvar w3m-search-engine-alist)
     (defvar w3m-search-default-engine))
 
   :init
   (setq browse-url-browser-function 'w3m-browse-url)
   (setq mm-text-html-renderer 'w3m)
-  (setq w3m-search-default-engine "duckduckgo"))
+
+  :config
+  (setq w3m-search-default-engine "duckduckgo")
+  (setq w3m-search-engine-alist
+    '(("duckduckgo" "https://duckduckgo.com/lite?q=%s" utf-8))))
