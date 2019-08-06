1e17ae85103fcf64041550502c408f6fb739d005
Kridsada Thanabulpong <sirn@ogsite.net>
2019-08-06 15:52:28 +0000

    [system] nfsd is gross

--- libexec/bootstrap/system_freebsd.sh
+++ libexec/bootstrap/system_freebsd.sh
@@ -66,67 +66,8 @@ _setup_pf() {
 "
 }
 
-_setup_nfsd() {
-    printe_h2 "Setting up nfsd..."
-
-    if [ ! -f /etc/exports ]; then
-        printe_info "/etc/exports is not configured, skipping..."
-        return
-    fi
-
-    netif=$(get_netif)
-    pfnfsd=/usr/local/libexec/pfnfsd.sh
-    nfscron=/usr/local/libexec/pfnfsd_periodic.sh
-
-    run_root sysrc nfs_server_enable=YES
-    run_root sysrc mountd_enable=YES
-    run_root sysrc rpcbind_enable=YES
-    run_root sysrc rpc_lockd_enable=YES
-    run_root sysrc rpc_statd_enable=YES
-
-    run_root service nfsd onestart
-    run_root service mountd onestart
-    run_root service rpcbind onestart
-
-    printe_h2 "Installing pfnfsd crontab..."
-
-    # Having root executing script from a user directory sounds dangerous
-    # so let's install nfscron into libexec first.
-    if forced || [ ! -f "$pfnfsd" ]; then
-        run_root mkdir -p "$(dirname $pfnfsd)"
-        run_root cp "$BASE_DIR/libexec/pfnfsd/pfnfsd.sh" $pfnfsd
-        run_root chown root:wheel $pfnfsd
-        run_root chmod 0700 $pfnfsd
-    fi
-
-    if forced || [ ! -f "$nfscron" ]; then
-        run_root mkdir -p "$(dirname $nfscron)"
-        run_root cp "$BASE_DIR/libexec/pfnfsd/periodic.sh" $nfscron
-        run_root chown root:wheel $nfscron
-        run_root chmod 0700 $nfscron
-    fi
-
-    cronline="@reboot $nfscron $netif"
-    tmpcron=$BUILD_DIR/crontab.pfnfsd
-    run_root crontab -u root -l > "$tmpcron" 2>/dev/null || true
-
-    lineinfile \
-        -f "$tmpcron" \
-        -l "$cronline" \
-        -r pfnfsd \
-        -s present
-
-    run_root crontab -u root - < "$tmpcron"
-    run_root sh $nfscron "$netif"
-    printe_info "pfnfsd crontab successfully installed"
-}
-
 _run() {
     _setup_pf
-}
-
-_run_dev() {
-    _setup_nfsd
 }
 
 run_with_flavors "$@"
