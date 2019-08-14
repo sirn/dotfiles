afba27d000c5f023d8b59fe8b66eb19cb8652445
Kridsada Thanabulpong <sirn@ogsite.net>
2019-08-14 16:42:14 +0000

    [etc/emacs] make flycheck virtualenv-aware

--- etc/emacs/packages/lang-python.el
+++ etc/emacs/packages/lang-python.el
@@ -1,7 +1,25 @@
 (eval-when-compile
   (defvar python-shell-interpreter))
 
+
 (setq python-shell-interpreter "python3")
+
+;; Taken from:
+;; https://github.com/lunaryorn/.emacs.d/blob/master/lisp/flycheck-virtualenv.el)
+
+(declare-function python-shell-calculate-exec-path "python")
+
+(defun gr/flycheck-venv-executable-find (executable)
+  "Find an EXECUTABLE in the current virtualenv if any."
+  (if (bound-and-true-p python-shell-virtualenv-root)
+      (let ((exec-path (python-shell-calculate-exec-path)))
+        (executable-find executable))
+    (executable-find executable)))
+
+(defun gr/flycheck-venv-setup ()
+  "Setup Flycheck for the current virtualenv."
+  (setq-local flycheck-executable-find 'gr/flycheck-venv-executable-find))
+
 
 (use-package anaconda-mode
   :commands (anaconda-mode anaconda-eldoc-mode)
@@ -10,6 +28,7 @@
 
   :init
   (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'gr/flycheck-venv-setup)
   (with-eval-after-load 'eldoc
     (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
 
