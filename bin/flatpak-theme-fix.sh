e659d8fbb7eb45d538e5f599ecb4e0e6b4e5c05c
Kridsada Thanabulpong <sirn@ogsite.net>
2019-08-31 03:29:23 +0700

    [bin/flatpak-theme-fix] initial version

---
+++ bin/flatpak-theme-fix.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Based on abiosoft's fixflatpaktheme.sh
+# https://github.com/abiosoft/dotfiles/blob/master/flatpak/fixflatpaktheme.sh
+#
+
+for appdir in $HOME/.var/app/*/; do
+    if [ ! -d "$appdir" ]; then
+        continue
+    fi
+
+    printf >&2 "==> Fixing %s...\\n" "$(basename "$appdir")"
+
+    for file in gtk-3.0/settings.ini fontconfig/fonts.conf; do
+        if [ -f "$HOME/.config/$file" ]; then
+            mkdir -p "${appdir}config/$(dirname "$file")"
+            cp "$HOME/.config/$file" "${appdir}config/$file"
+        fi
+    done
+done
