c6bef159894ee0065cd6b72235672d20fe4456a9
Kridsada Thanabulpong <sirn@ogsite.net>
2020-01-29 15:35:00 +0900

    [pkgs/python] remove cryptography patch and update pip

--- libexec/packages/python.sh
+++ libexec/packages/python.sh
@@ -12,53 +12,6 @@ cd "$(dirname "$0")" || exit 1
 PYTOOLS=${XDG_DATA_HOME:-$HOME/.local/share}/pytools
 PIP=$PYTOOLS/bin/pip3
 
-# py-cryptography doesn't build under LibreSSL (e.g. OpenBSD, Void)
-# without patching the CFFI source to disable some OpenSSL
-# features.
-
-PYCRYPT_VER=2.8
-PYCRYPT_SHA256=074e45d510156b3b45e0ffadf57a9655089922fdf84f611b239bb7732ccc18ee
-PYCRYPT_OPENBSD_PATCH_COMMIT=e4337acb2f751d05d57fa4308df8065e886e025f
-PYCRYPT_OPENBSD_PATCHES="\
-security/py-cryptography/patches/patch-src__cffi_src_openssl_x509_vfy_py
-"
-
-_maybe_install_cryptography() {
-    if ! forced && $PIP show cryptography==$PYCRYPT_VER >/dev/null 2>&1; then
-        return
-    fi
-
-    case $(openssl version | tr '[:upper:]' '[:lower:]') in
-        libressl* )
-            :
-            ;;
-
-        * )
-            return
-            ;;
-    esac
-
-    printe_h2 "Patching py-cryptography for libressl..."
-    cd "$BUILD_DIR" || exit 1
-
-    fetch_gh_archive cryptography.tar.gz pyca/cryptography $PYCRYPT_VER
-    verify_shasum cryptography.tar.gz $PYCRYPT_SHA256
-    tar -C "$BUILD_DIR" -xzf cryptography.tar.gz
-    rm cryptography.tar.gz
-
-    cd "$BUILD_DIR/cryptography-$PYCRYPT_VER" || exit 1
-
-    for filename in $PYCRYPT_OPENBSD_PATCHES; do
-        fetch_gh_raw - \
-                     openbsd/ports \
-                     "$PYCRYPT_OPENBSD_PATCH_COMMIT" \
-                     "$filename" |
-            patch -p0
-    done
-
-    $PIP install .
-}
-
 _run() {
     if ! command -v python3 >/dev/null; then
        printe_h2 "python3 is not installed, skipping python packages..."
@@ -84,15 +37,14 @@ _run() {
 }
 
 _run_dev() {
-    _maybe_install_cryptography
-
     printe_h2 "Installing python dev packages..."
 
-    $PIP install \
+    $PIP install --upgrade \
          ansible \
          black \
          flake8 \
          ipwhois \
+         kapitan \
          pip \
          poetry \
          pre-commit \
