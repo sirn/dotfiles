ba6cb0f60edba629319e4a4599ae863dec0765f0
Kridsada Thanabulpong <sirn@ogsite.net>
2020-03-12 01:30:54 +0900

    emacs: remove pcomplete for eshell

--- etc/emacs/packages/editor-term.el
+++ etc/emacs/packages/editor-term.el
@@ -3,8 +3,21 @@
 (use-feature eshell
   :commands eshell
 
+  :preface
+  (eval-when-compile
+    (declare-function gemacs--eshell-remove-pcomplete nil))
+
   :leader
-  ("'" #'eshell))
+  ("'" #'eshell)
+
+  :config
+  (defun gemacs--eshell-remove-pcomplete ()
+    "Remove deprecated company hook from eshell."
+    (remove-hook
+      'completion-at-point-functions
+      #'pcomplete-completions-at-point t))
+
+  (add-hook 'eshell-mode-hook #'gemacs--eshell-remove-pcomplete))
 
 
 (use-package multi-term
