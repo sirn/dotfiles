53732f28cc6cbd6ba27cef0f0390d5b4a3b8e8a7
Kridsada Thanabulpong <sirn@ogsite.net>
2020-05-16 08:33:15 +0900

    emacs: disable editorconfig/dtrt-indent for makefile

---
+++ etc/emacs/packages/lang-makefile.el
@@ -0,0 +1,28 @@
+;; -*- lexical-binding: t -*-
+
+(use-feature makefile
+  :preface
+  (eval-when-compile
+    (declare-function gemacs--makefile-force-tabs nil)
+    (declare-function gemacs--makefile-disable-editorconfig nil)
+    (declare-function gemacs--makefile-disable-dtrt-indent nil))
+
+  :config
+  (use-feature editorconfig-mode
+    :config
+    (defun gemacs--makefile-disable-editorconfig ()
+      "Disable editorconfig-mode for Makefile."
+      (editorconfig-mode -1))
+    (add-hook 'makefile-mode-hook #'gemacs--makefile-disable-editorconfig))
+
+  (use-feature dtrt-indent
+    :config
+    (defun gemacs--makefile-disable-dtrt-indent ()
+      "Disable dtrt-indent-mode for Makefile."
+      (dtrt-indent-mode -1))
+    (add-hook 'makefile-mode-hook #'gemacs--makefile-disable-dtrt-indent))
+
+  (defun gemacs--makefile-force-tabs ()
+    "Force Makefile to always use tabs."
+    (setq indent-tabs-mode t))
+  (add-hook 'makefile-mode-hook #'gemacs--makefile-force-tabs))
