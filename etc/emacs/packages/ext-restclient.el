84eaf5dc72f92fdefe0d4ab8f5783a3b5f6f0847
Kridsada Thanabulpong <sirn@ogsite.net>
2020-05-23 00:17:58 +0900

    emacs: add ob-restclient

--- etc/emacs/packages/ext-restclient.el
+++ etc/emacs/packages/ext-restclient.el
@@ -8,3 +8,13 @@
   :demand t
   :config
   (add-to-list 'company-backends 'company-restclient))
+
+
+(use-package ob-restclient
+  :config
+  (use-feature org
+    :config
+    (org-babel-do-load-languages
+      'org-label-load-languages
+      (append org-babel-load-languages
+        '((restclient . t))))))
