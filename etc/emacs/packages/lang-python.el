fb75f2ded8f5c6d8979a9a39d325674d9e92a956
Kridsada Thanabulpong <sirn@ogsite.net>
2020-06-10 16:34:42 +0900

    bin: better wrapper bin paths

--- etc/emacs/packages/lang-python.el
+++ etc/emacs/packages/lang-python.el
@@ -11,7 +11,7 @@
   (setq lsp-pyls-server-command
     (gemacs--path-join
       (file-name-as-directory (getenv "HOME"))
-      ".dotfiles/bin/pyls"))
+      ".dotfiles/libexec/lsp/pyls"))
 
   :config
   (defun gemacs--python-fix-outline-mode-config ()
