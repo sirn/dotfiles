c5cfa5caefe57b453227bcacd051a2defb9071e2
Kridsada Thanabulpong <sirn@ogsite.net>
2020-09-07 15:39:14 +0900

    emacs: remove nimscript workaround

--- etc/emacs/packages/lang-nim.el
+++ etc/emacs/packages/lang-nim.el
@@ -1,21 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package nim-mode
-  :preface
-  (eval-when-compile
-    (declare-function gemacs--nimscript-mode-maybe nil))
-
   :config
-  (advice-add 'nimscript-mode-maybe :around #'gemacs--nimscript-mode-maybe)
-
-  (defun gemacs--nimscript-mode-maybe (_func &rest _args)
-    "Fix NimScript detection. The original `nimscript-mode-maybe'
-were causing `nim-mode' to always error with `Wrong type argument'.
-
-This function is meant to workaround such issue by force enabling
-`nimscript-mode' all the time when visiting nims or nimble file."
-    (nimscript-mode))
-
   (use-feature lsp
     :config
     (add-hook 'nim-mode-hook #'lsp)))
