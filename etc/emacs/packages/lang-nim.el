7a3135b2647122bc15f22433681134470d9818ca
Kridsada Thanabulpong <sirn@ogsite.net>
2020-09-07 21:56:20 +0900

    emacs: fix nim-mode-forward-token

--- etc/emacs/packages/lang-nim.el
+++ etc/emacs/packages/lang-nim.el
@@ -2,6 +2,21 @@
 
 (use-package nim-mode
   :config
+  (el-patch-defun nim-mode-forward-token ()
+    "Handle cases where `nim-smie--line-info' is `nil'."
+    (when (line-number-at-pos)
+      (let ((_pos (point)))
+        (skip-chars-forward " \t")
+        (forward-comment (point-max))
+        (let* ((tok (smie-default-forward-token)))
+          (if (< (el-patch-wrap 1 1
+                   (or
+                     (assoc-default :line nim-smie--line-info)
+                     -1))
+                 (line-number-at-pos))
+              (setq tok ";"))
+          tok))))
+
   (use-feature lsp
     :config
     (add-hook 'nim-mode-hook #'lsp)))
