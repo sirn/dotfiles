449ad2ee87c0e70fa65c4b302bd5dc47b062b21c
Kridsada Thanabulpong <sirn@ogsite.net>
2022-01-11 19:32:47 +0900

    pkgs/net/postgres: fix darwin build

--- libexec/packages/net/postgres.sh
+++ libexec/packages/net/postgres.sh
@@ -25,6 +25,16 @@ _run() {
 }
 
 _install_postgres() {
+    case "$(uname)" in
+        Darwin )
+            POSTGRES_EXTRA_CONFIGURE_OPTIONS="\
+--with-includes=/opt/local/include/openssl-1.1 \
+--with-libraries=/opt/local/lib/openssl-1.1\
+"
+            export POSTGRES_EXTRA_CONFIGURE_OPTIONS
+            ;;
+    esac
+
     asdf_plugin postgres https://github.com/smashedtoatoms/asdf-postgres
     asdf_install postgres "$POSTGRES_VERSION" global
 }
