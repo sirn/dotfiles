24cb3e2269fdc18375a2d562e4869cf41072a029
Kridsada Thanabulpong <sirn@ogsite.net>
2022-02-13 03:32:15 +0900

    emacs: bring sanity to kubernetes yaml

--- etc/emacs/packages/lang-yaml.el
+++ etc/emacs/packages/lang-yaml.el
@@ -1,15 +1,13 @@
 ;; -*- lexical-binding: t -*-
 
-(use-package yaml-mode)
-
-
-(use-package ansible
+(use-package yaml-mode
   :preface
   (eval-when-compile
-    (declare-function gemacs--ansible-maybe-enable nil))
+    (declare-function gemacs--yaml-maybe-k8s nil)
+    (declare-function gemacs--yaml-maybe-ansible nil))
 
   :init
-  (defun gemacs--ansible-maybe-enable ()
+  (defun gemacs--yaml-maybe-ansible ()
     (when (and
             (stringp buffer-file-name)
             (string-match
@@ -20,18 +18,39 @@
                       "/" (* nonl)))
                 ".y" (? "a") "ml")
               buffer-file-name))
-      (ansible t)))
+      (ansible +1)))
 
-  (add-hook 'yaml-mode-hook 'gemacs--ansible-maybe-enable))
+  (defun gemacs--yaml-maybe-k8s ()
+    (when (and
+            (stringp buffer-file-name)
+            (string-match
+              (rx ".yaml" string-end)
+              buffer-file-name)
+            (save-excursion
+              (goto-char (point-min))
+              (looking-at "\\(---\n\\)?apiVersion:")))
+       (apheleia-mode -1)
+       (flycheck-mode -1)))
+
+  (add-hook 'yaml-mode-hook 'gemacs--yaml-maybe-ansible)
+  (add-hook 'yaml-mode-hook 'gemacs--yaml-maybe-k8s))
+
+
+(use-package ansible)
 
 
 (use-package ansible-doc)
 
 
 (use-package company-ansible
+  :preface
+  (eval-when-compile
+    (declare-function gemacs--company-ansible-setup nil))
+
   :init
   (use-feature company
     :config
-    (defun gemacs--setup-company-ansible ()
+    (defun gemacs--company-ansible-setup ()
       (set (make-local-variable 'company-backends) '(company-ansible)))
-    (add-hook 'ansible-hook #'gemacs--setup-company-ansible)))
+
+    (add-hook 'ansible-hook #'gemacs--company-ansible-setup)))
