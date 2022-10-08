ffb895ca707566618f07cda5c5da684fe69e5658
Sirn Thanabulpong <sirn@ogsite.net>
2022-10-09 04:33:15 +0900

    emacs: add command to sync projects
    gemacs--project-sync matching the behavior with gg command in oksh.
    
    even though 'project-remember-projects-under` exists, it doesn't
    remove matched directory prior to walking further, which cause
    .git/.hg in sub-directory to get added to projects list.
    
    our version which uses `find' is also much faster. able to scan
    through ~14,000 directories in less than a second.

--- etc/emacs/packages/editor-project.el
+++ etc/emacs/packages/editor-project.el
@@ -6,6 +6,7 @@
 
   :general
   (leader
+    "pSR" #'gemacs--project-sync
     "pf" #'project-find-file
     "pp" #'project-switch-project
     "pb" #'project-switch-to-buffer
@@ -18,6 +19,7 @@
   (eval-when-compile
     (declare-function ag-project-regexp nil)
     (declare-function gemacs--project-ag nil)
+    (declare-function gemacs--project-sync nil)
     (declare-function leader nil)
     (declare-function project-async-shell-command nil)
     (declare-function project-compile nil)
@@ -37,4 +39,41 @@
         (call-interactively 'ag-project-regexp)))
 
     (general-with-eval-after-load 'general
-      (leader "p/" #'gemacs--project-ag))))
+      (leader "p/" #'gemacs--project-ag)))
+
+  (defun gemacs--project-sync ()
+    (interactive)
+    (project-forget-zombie-projects)
+    (let* ((codedirs
+            (seq-remove #'string-empty-p
+                          (split-string
+                           (shell-command-to-string "git config --get-all ghq.root")
+                           "\n")))
+           (projdirs
+            (seq-remove
+             #'string-empty-p
+             (flatten-tree
+              (seq-map
+               (lambda (codedir)
+                 (let ((realcodedir (expand-file-name codedir)))
+                   (when (file-directory-p realcodedir)
+                     (split-string
+                      (shell-command-to-string
+                       (mapconcat
+                        #'shell-quote-argument
+                        ;; Note: command should match with `gg' in `etc/ksh/kshrc'.
+                        `("find" ,realcodedir
+                          "-not" "-path" "*/node_modules/*"
+                          "-not" "-path" "*/vendor/*"
+                          "("
+                          "-exec" "test" "-d" "{}/.git" ";"
+                          "-or" "-exec" "test" "-d" "{}/.hg" ";"
+                          ")"
+                          "-print" "-prune")
+                        " "))
+                      "\n"))))
+               codedirs)))))
+      (seq-doseq (projdir projdirs)
+        (let* ((dir (abbreviate-file-name projdir))
+               (pr (project-current nil dir)))
+          (project-remember-project pr t))))))
