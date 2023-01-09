b8b2f85b700c8102838c7103f1dd181832679e76
Sirn Thanabulpong <sirn@ogsite.net>
2023-01-09 10:12:37 +0100

    home-manager: add amethyst config

--- nixpkgs/modules/home/links.nix
+++ nixpkgs/modules/home/links.nix
@@ -1,16 +1,25 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+with lib;
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
+  inherit (pkgs.stdenv) isLinux isDarwin;
 
   dotfilesDir = "${config.home.homeDirectory}/.dotfiles";
   dotprivDir = "${config.home.homeDirectory}/.dotpriv";
 in
 {
-  home.file = {
-    ".hgrc" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/hg/hgrc"; };
-    ".kshrc" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/ksh/kshrc"; };
-    ".profile" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/sh/profile"; };
-    ".ssh/config" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/ssh/config"; };
-  };
+  home.file = mkMerge [
+    {
+      ".hgrc" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/hg/hgrc"; };
+      ".kshrc" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/ksh/kshrc"; };
+      ".profile" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/sh/profile"; };
+      ".ssh/config" = { source = mkOutOfStoreSymlink "${dotfilesDir}/etc/ssh/config"; };
+    }
+    (mkIf (isDarwin && config.machine.gui.enable) {
+      ".amethyst.yml" = {
+        source = mkOutOfStoreSymlink "${dotfilesDir}/etc/amethyst/amethyst.yml";
+      };
+    })
+  ];
 }
