90f5342ab92363d8e7764b11e793efb816772780
Sirn Thanabulpong <sirn@ogsite.net>
2023-10-09 03:58:34 +0900

    home-manager/amethyst: fix syntax

--- nixpkgs/modules/programs/amethyst.nix
+++ nixpkgs/modules/programs/amethyst.nix
@@ -3,7 +3,7 @@
 {
   home.file = {
     ".amethyst.yml" = {
-      text = lib.generators.toYAML {
+      text = lib.generators.toYAML {} {
         layouts = [ "tall" "wide" "fullscreen" "column" ];
         mouse-follows-focus = false;
         focus-follows-mouse = false;
