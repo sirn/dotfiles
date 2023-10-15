48952b1e00c0a131814c4388ea8b2dc6fb90acb2
Sirn Thanabulpong <sirn@ogsite.net>
2023-10-15 20:50:40 +0900

    hm/flatpak: manage on phoebe/vega as well

--- nixpkgs/modules/machines/vega.nix
+++ nixpkgs/modules/machines/vega.nix
@@ -11,13 +11,16 @@
 
     # programs
     ../programs/emacsc.nix
+    ../programs/firefox.nix
     ../programs/foot.nix
     ../programs/fuzzel.nix
     ../programs/intellij.nix
     ../programs/kanshi.nix
     ../programs/looking-glass-client.nix
     ../programs/mpv.nix
+    ../programs/obsidian.nix
     ../programs/sway.nix
+    ../programs/thunderbird.nix
     ../programs/waybar.nix
 
     # services
