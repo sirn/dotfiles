cccdb92688fd2ba7648a3cb57d5d85352f16c081
Sirn Thanabulpong <sirn@ogsite.net>
2023-10-15 20:39:21 +0900

    hm/kanshi: separate profiles for single xdr and lg

--- nixpkgs/modules/programs/kanshi.nix
+++ nixpkgs/modules/programs/kanshi.nix
@@ -14,14 +14,20 @@
       {
         text = ''
           profile main_dual {
-            output "${apple_pro_xdr_display}" mode 6016x3384 position 2194,0 scale 2
-            output "${lg_27uk650_w}" mode 3840x2160 position 0,0 scale 1.75
+            output "${apple_pro_xdr_display}" position 2194,0 scale 2
+            output "${lg_27uk650_w}" position 0,0 scale 1.5
             exec swaymsg workspace 1, move workspace to output '"${apple_pro_xdr_display}"'
+            exec swaymsg workspace 10, move workspace to output '"${lg_27uk650_w}"'
           }
 
-          profile main_single {
-            output "${lg_27uk650_w}" mode 3840x2160 position 0,0 scale 1.75
+          profile main_single_lg {
+            output "${lg_27uk650_w}" position 0,0 scale 1.5
             exec swaymsg workspace 1, move workspace to output '"${lg_27uk650_w}"'
+          }
+
+          profile main_single_xdr {
+            output "${apple_pro_xdr_display}" position 0,0 scale 2
+            exec swaymsg workspace 1, move workspace to output '"${apple_pro_xdr_display}"'
           }
         '';
       };
