c9f773ed0ddb44a79220035a6dedd72074e339c9
Sirn Thanabulpong <sirn@ogsite.net>
2023-11-09 22:34:52 +0900

    hm/looking-glass-client: use os package on non-Nix

--- nixpkgs/modules/programs/looking-glass-client.nix
+++ nixpkgs/modules/programs/looking-glass-client.nix
@@ -1,6 +1,21 @@
+{ config, pkgs, ... }:
+
 {
   programs.looking-glass-client = {
     enable = true;
+
+    # Looking Glass requires EGL, which doesn't work when versions between
+    # Nix and the host mismatched. Only enable for NixOS. Also,
+    # looking-glass-client.package can't use null.
+    package =
+      if config.machine.nixos.enable
+      then pkgs.looking-glass-client
+      else
+        pkgs.writeTextFile {
+          name = "looking-glass-client";
+          text = "install looking-glass using os package manager";
+          destination = "/var/looking-glass-client";
+        };
 
     settings = {
       input = {
