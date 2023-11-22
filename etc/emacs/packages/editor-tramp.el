bcd227596a2a692bd96da49a9c364179ec3d03f7
Sirn Thanabulpong <sirn@ogsite.net>
2023-11-22 10:38:44 +0900

    emacs: fix doas in tramp

--- etc/emacs/packages/editor-tramp.el
+++ etc/emacs/packages/editor-tramp.el
@@ -7,4 +7,15 @@
   (tramp-use-ssh-controlmaster-options nil)
 
   :config
-  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+  ;; https://adam.kruszewski.name/2023-05-07-opondoas-emacs-tramp-and-gentoo.html
+  (add-to-list 'tramp-methods
+    `("doas"
+       (tramp-login-program         "doas")
+       (tramp-login-args            (("-u" "%u") ("su - %u")))
+       (tramp-remote-shell          ,tramp-default-remote-shell)
+       (tramp-remote-shell-args     ("-c"))
+       (tramp-connection-timeout    10)
+       (tramp-session-timeout       300)
+       (tramp-password-previous-hop t))))
