e44f6e71dca02991150017d99097d05a00845358
Sirn Thanabulpong <sirn@ogsite.net>
2023-11-26 18:34:30 +0900

    emacs: drop ts-mode for shell

--- etc/emacs/packages/lang-sh.el
+++ etc/emacs/packages/lang-sh.el
@@ -6,6 +6,5 @@
     (declare-function apheleia-mode nil))
 
   :init
-  (add-to-list 'major-mode-remap-alist '(sh-mode . bash-ts-mode))
   (add-hook 'sh-base-mode-hook #'apheleia-mode)
   (add-hook 'sh-base-mode-hook #'flycheck-mode))
