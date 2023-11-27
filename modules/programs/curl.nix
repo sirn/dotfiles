86078fc2181f0b535477cbdb067309a05e68bd6b
Sirn Thanabulpong <sirn@ogsite.net>
2023-11-27 12:17:48 +0900

    hm/curl: enable http3, brotli, zstd, etc.

--- modules/programs/curl.nix
+++ modules/programs/curl.nix
@@ -2,6 +2,12 @@
 
 {
   home.packages = with pkgs; [
-    curlFull
+    (curl.override {
+      brotliSupport = true;
+      http3Support = true;
+      idnSupport = true;
+      openssl = pkgs.quictls;
+      zstdSupport = true;
+    })
   ];
 }
