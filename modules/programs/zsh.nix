f51918a4fda6c13a9a2814f27910b1051aa3fcd9
Sirn Thanabulpong <sirn@ogsite.net>
2024-04-09 17:05:02 +0900

    hm/zsh: only set SHELL for interactive shell

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -21,8 +21,10 @@ in
     initExtra = ''
       bindkey -e
 
-      export SHELL=zsh
       export WORDCHARS="''${WORDCHARS/\//}"
+      if [[ $- == *"i"* ]]; then
+        export SHELL=${config.programs.zsh.package}/bin/zsh
+      fi
 
       . ${dotfilesDir}/etc/zsh/share/ps1.zsh
       . ${dotfilesDir}/etc/zsh/functions/cd.zsh
