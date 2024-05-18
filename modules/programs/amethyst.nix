dd6130779f718a2e531c650ce94edfd46bd0bdff
Sirn Thanabulpong <sirn@ogsite.net>
2024-05-18 16:20:56 +0900

    hm/amethyst: focus follow mouse and floating

--- modules/programs/amethyst.nix
+++ modules/programs/amethyst.nix
@@ -3,10 +3,17 @@
 {
   home.file = {
     ".amethyst.yml" = {
-      text = lib.generators.toYAML {} {
+      text = lib.generators.toYAML { } {
         layouts = [ "tall" "wide" "fullscreen" "column" ];
-        mouse-follows-focus = false;
-        focus-follows-mouse = false;
+        mouse-follows-focus = true;
+        focus-follows-mouse = true;
+
+        floating = [
+          "com.adobe.illustrator"
+          "com.adobe.InDesign"
+          "com.adobe.Photoshop"
+          "org.blenderfoundation.blender"
+        ];
       };
     };
   };
