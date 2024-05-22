4f578fc11651bdc79e8d882402c2538517d5b6a7
Sirn Thanabulpong <sirn@ogsite.net>
2024-05-22 14:18:32 +0900

    hm/podman: alias to docker/docker-compose

--- modules/programs/podman.nix
+++ modules/programs/podman.nix
@@ -8,11 +8,29 @@ in
   home.packages = with pkgs; [
     skopeo
   ] ++
-  (if isLinux then [ podman ] else [ ]) ++
+  (if isLinux then [
+    podman
+    podman-compose
+
+    (pkgs.writeScriptBin "docker" ''
+      #!${pkgs.bash}/bin/bash
+      exec ${podman}/bin/podman "$@"
+    '')
+
+    (pkgs.writeScriptBin "docker-compose" ''
+      #!${pkgs.bash}/bin/bash
+      exec ${podman-compose}/bin/podman-compose "$@"
+    '')
+  ] else [ ]) ++
   (if isDarwin then [
     (pkgs.writeScriptBin "podman" ''
       #!${pkgs.bash}/bin/bash
       exec /usr/local/bin/docker "$@"
+    '')
+
+    (pkgs.writeScriptBin "podman-compose" ''
+      #!${pkgs.bash}/bin/bash
+      exec /usr/local/bin/docker-compose "$@"
     '')
   ] else [ ]);
 }
