4b75cdb4c85347d32161b6e4febd98d844ab91ee
Sirn Thanabulpong <sirn@ogsite.net>
2024-07-10 19:55:04 +0900

    hm/emacsc: force nw

--- modules/programs/emacsc.nix
+++ modules/programs/emacsc.nix
@@ -6,7 +6,7 @@ let
   emacsc = pkgs.writeScriptBin "emacsc" ''
     #!${pkgs.bash}/bin/bash
     # Run Emacs Client on console
-    exec ${config.programs.emacs.finalPackage}/bin/emacsclient --alternate-editor= --create-frame "$@"
+    exec ${config.programs.emacs.finalPackage}/bin/emacsclient -nw --alternate-editor= --create-frame "$@"
   '';
 in
 {
