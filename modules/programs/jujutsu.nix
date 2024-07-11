436766847ed8e04a0e9f8c7d1cc2b214511c4aa3
Sirn Thanabulpong <sirn@ogsite.net>
2024-07-12 04:29:42 +0900

    programs/jujutsu: completion

--- modules/programs/jujutsu.nix
+++ modules/programs/jujutsu.nix
@@ -36,4 +36,20 @@ in
       source = config.xdg.configFile."jj/config.toml".source;
     };
   };
+
+  # Shell completion based on
+  # https://github.com/martinvonz/jj/blob/0690922ca15ced55e417edab806c982b0cc42b84/docs/install-and-setup.md
+  programs.bash.initExtra = ''
+    source <(${config.programs.jujutsu.package}/bin/jj util completion bash)
+  '';
+
+  programs.zsh.initExtra = ''
+    autoload -U compinit
+    compinit
+    source <(${config.programs.jujutsu.package}/bin/jj util completion zsh)
+  '';
+
+  programs.fish.interactiveShellInit = ''
+    ${config.programs.jujutsu.package}/bin/jj util completion fish | source
+  '';
 }
