ea55d0dbd5d199ecc30bd875c9fc247b4cc36d48
Sirn Thanabulpong <sirn@ogsite.net>
2024-08-27 20:38:56 +0900

    programs/zed: configure

--- modules/programs/zed.nix
+++ modules/programs/zed.nix
@@ -1,7 +1,23 @@
 { pkgs, ... }:
 
+let
+  inherit (pkgs) formats;
+  jsonFormat = formats.json { };
+in
 {
   home.packages = with pkgs; [
     unstable.zed-editor
   ];
+
+  xdg.configFile = {
+    "zed/settings.json" = {
+      source = jsonFormat.generate "settings.json" {
+        theme = "Rosé Pine";
+        vim_mode = true;
+        ui_font_size = 16;
+        buffer_font_size = 16;
+        auto_update = false;
+      };
+    };
+  };
 }
