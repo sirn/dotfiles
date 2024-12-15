abacf67c669850505e9554e4ff28663cec300a8f
Sirn Thanabulpong <sirn@ogsite.net>
2024-12-15 17:42:56 +0900

    pkgs: simplify pkgs structure

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,12 +8,12 @@ let
   callPackageUnstable = callPackageWith (pkgs.unstable);
 in
 {
-  inherit (recurseIntoAttrs (callPackage ./data/fonts/ia-fonts { }))
+  inherit (recurseIntoAttrs (callPackage ./by-name/ia-fonts { }))
     ia-writer-duo-static
     ia-writer-mono-static
     ia-writer-quattro-static;
 
-  inherit (recurseIntoAttrs (callPackage ./applications/editors/emacs { }))
+  inherit (recurseIntoAttrs (callPackage ./by-name/emacs { }))
     emacs
     emacs-nox
     emacs-pgtk
@@ -21,22 +21,22 @@ in
     emacs-macport;
 
   emacsPackages = {
-    apheleia = (callPackage ./applications/editors/emacs/elisp-packages/apheleia { });
+    apheleia = (callPackage ./by-name/emacs/elisp-packages/apheleia { });
 
-    sqlite3 = (callPackage ./applications/editors/emacs/elisp-packages/sqlite3 { });
+    sqlite3 = (callPackage ./by-name/emacs/elisp-packages/sqlite3 { });
 
-    visual-regexp-steroids = (callPackage ./applications/editors/emacs/elisp-packages/visual-regexp-steroids { });
+    visual-regexp-steroids = (callPackage ./by-name/emacs/elisp-packages/visual-regexp-steroids { });
   };
 
-  firefox-csshacks = (callPackage ./applications/networking/browsers/firefox/csshacks.nix { });
+  firefox-csshacks = (callPackage ./by-name/firefox/csshacks.nix { });
 
-  looking-glass-client_b6 = (callPackage ./applications/virtualization/looking-glass-client/b6.nix { });
+  looking-glass-client_b6 = (callPackage ./by-name/looking-glass-client/b6.nix { });
 
-  looking-glass-client_b7 = (callPackage ./applications/virtualization/looking-glass-client/b7.nix { });
+  looking-glass-client_b7 = (callPackage ./by-name/looking-glass-client/b7.nix { });
 
-  s-tui = (callPackage ./tools/system/s-tui { });
+  s-tui = (callPackage ./by-name/s-tui { });
 
-  unison-nox = (callPackage ./applications/networking/sync/unison-nox { });
+  unison-nox = (callPackage ./by-name/unison-nox { });
 
-  wl-clipboard = (callPackage ./tools/wayland/wl-clipboard/wrapped.nix { });
+  wl-clipboard = (callPackage ./by-name/wl-clipboard/wrapped.nix { });
 }
