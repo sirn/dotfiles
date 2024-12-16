9a0df7bf2bfde589c2aede301fbb4e66a4d88ff1
Sirn Thanabulpong <sirn@ogsite.net>
2024-12-16 15:31:11 +0900

    programs/ffmpeg: enable libvpl
    I don't run anything older than Intel 11 Gen so it should be safe to
    disable intel-media-sdk support in ffmpeg and enable libVpl instead.
    
    This would also allow us to use av1_qsv in supported platforms
    such as Lunar Lake, Alchemist, Battlemage, etc.

--- modules/programs/ffmpeg.nix
+++ modules/programs/ffmpeg.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  inherit (lib) mkIf;
+  inherit (pkgs.stdenv) isLinux;
+in
 {
   home.packages = with pkgs; [
-    ffmpeg-full
+    (ffmpeg-full.override (if isLinux then {
+      withMfx = false;
+      withVpl = true;
+    } else { }))
   ];
 }
