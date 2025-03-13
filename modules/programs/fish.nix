1361db5773b887af3466223748ca617a03c25ede
Sirn Thanabulpong <sirn@ogsite.net>
2025-03-14 00:21:59 +0900

    fish: make sure jj prompt overrides git

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -46,10 +46,13 @@ in
 
       fish_vcs_prompt = {
         body = ''
-          fish_git_prompt $argv
-          or fish_hg_prompt $argv
+          ${if config.programs.jujutsu.enable then ''
+            fish_jj_prompt $argv
+            or fish_git_prompt $argv
+          '' else ''
+            fish_git_prompt $argv
+          ''} or fish_hg_prompt $argv
           or fish_fossil_prompt $argv
-          ${if config.programs.jujutsu.enable then "or fish_jj_prompt $argv" else ""}
         '';
       };
 
