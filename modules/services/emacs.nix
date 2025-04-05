aa33999c7ae9465e8f7241d9354866e8b192d446
Sirn Thanabulpong <sirn@ogsite.net>
2025-04-05 13:34:18 +0900

    services/emacs: restore launchd service

--- modules/services/emacs.nix
+++ modules/services/emacs.nix
@@ -1,9 +1,25 @@
+{ config, pkgs, lib, ... }:
+
 {
   services.emacs = {
-    enable = true;
+    enable = pkgs.stdenv.isLinux;
 
     socketActivation = {
       enable = true;
+    };
+  };
+
+  launchd.agents.emacs = {
+    enable = true;
+    config = {
+      RunAtLoad = true;
+      KeepAlive = true;
+      ProgramArguments = [
+        "/bin/sh"
+        "-l"
+        "-c"
+        "${config.programs.emacs.finalPackage}/bin/emacs --fg-daemon --chdir=$HOME"
+      ];
     };
   };
 }
