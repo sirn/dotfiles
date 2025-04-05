e135b04bfff5be0820f3d58cb67a1ad582bff9b0
Sirn Thanabulpong <sirn@ogsite.net>
2025-04-06 01:29:51 +0900

    services/gpg-agent: enable ssh support

--- modules/services/gpg-agent.nix
+++ modules/services/gpg-agent.nix
@@ -3,6 +3,7 @@
 {
   services.gpg-agent = {
     enable = config.programs.gpg.enable;
+    enableSshSupport = true;
 
     pinentryPackage = lib.mkDefault (
       if pkgs.stdenv.isDarwin
@@ -10,8 +11,15 @@
       else pkgs.pinentry-curses
     );
 
-    defaultCacheTtl = 86400;
-    maxCacheTtl = 86400;
+    defaultCacheTtl = 3600;
+    defaultCacheTtlSsh = 3600;
+    maxCacheTtl = 43200;
+    maxCacheTtlSsh = 43200;
+
+    # gpg --list-keys --with-keygrip
+    sshKeys = [
+      "DB2F6C327247DA184010A3181710D1C56B427410"
+    ];
 
     extraConfig = ''
       allow-emacs-pinentry
