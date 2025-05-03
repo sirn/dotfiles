c6646fde858741b9ae50d6606598c28b54ae3d1e
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-03 14:32:17 +0900

    programs/claude-code: add Claude Code configuration

--- modules/programs/claude-code.nix
+++ modules/programs/claude-code.nix
@@ -1,7 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  dotprivDir = "${config.home.homeDirectory}/.dotpriv";
+in
 {
   home.packages = [
     pkgs.local.claude-code
   ];
+
+  programs.git = {
+    ignores = [
+      "CLAUDE.local.md"
+    ];
+  };
+
+  home.file = {
+    ".claude/CLAUDE.md" = { 
+      source = config.lib.file.mkOutOfStoreSymlink "${dotprivDir}/etc/claude/CLAUDE.md";
+    };
+  };
 }
