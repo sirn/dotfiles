07f06cd6ea60fe07528e6f2647c9e1b88eba455e
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-06 12:51:02 +0900

    *: temporarily fix macos
    This reverts commit fa324fc600565d7f2b40df8c6323fec16429a505.

--- flake.nix
+++ flake.nix
@@ -27,6 +27,12 @@
 
       overlays = [
         (final: prev: {
+          # Track latest LTS
+          # https://nodejs.org/en/about/previous-releases
+          nodejs = prev.nodejs_22;
+          nodejs-slim = prev.nodejs-slim_22;
+        })
+        (final: prev: {
           local = import ./pkgs {
             pkgs = final;
             lib = prev.lib;
