085aa7feb4bddeb77f64e07e91e39ff2486725d3
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-06 23:37:45 +0900

    etc/emacs: use openrouter, some shortcut keys

--- etc/emacs/packages/editor-llm.el
+++ etc/emacs/packages/editor-llm.el
@@ -3,29 +3,31 @@
 (use-package gptel
   :defer 1
 
+  :general
+  (leader
+    "gg" #'gptel)
+
   :preface
   (eval-when-compile
-    (declare-function gptel-mode nil)
-    (declare-function auth-source-search nil)
-    (declare-function gemacs--gptel-api-key-from-auth-source nil))
+    (declare-function gptel-api-key-from-auth-source nil))
 
   :custom
   (gptel-default-mode 'org-mode)
-  (gptel-model 'claude-3-5-sonnet-20240620)
+  (gptel-model 'openai/gpt-4o-mini)
+  (gptel-backend (gptel-make-openai "OpenRouter"
+                   :host "openrouter.ai"
+                   :endpoint "/api/v1/chat/completions"
+                   :key #'gptel-api-key-from-auth-source
+                   :stream t
+                   :models
+                   '(anthropic/claude-3.7-sonnet
+                     google/gemini-2.5-pro-preview-03-25
+                     google/gemini-2.5-flash-preview
+                     openai/gpt-4o-mini)))
 
   :init
   (add-hook 'gptel-mode-hook #'visual-line-mode)
 
   :config
-  (defun gemacs--gptel-api-key-from-auth-source (host)
-    "Retrieve API key for HOST from auth-source."
-    (when-let ((secret (plist-get (car (auth-source-search :host host :max 1)) :secret)))
-      (if (functionp secret)
-          (funcall secret)
-        secret)))
-
-  (let ((api-key (gemacs--gptel-api-key-from-auth-source "anthropic.com")))
-    (setq gptel-backend (gptel-make-anthropic
-                         "Claude Sonnet"
-                         :key api-key
-                         :stream t))))
+  (general-define-key :keymaps 'gptel-mode-map
+    "C-c C-c" #'gptel-menu))
