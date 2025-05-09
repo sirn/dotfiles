ceb43a5f2c5731dc60e11f9b10e7fbc0187aa284
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-09 18:34:17 +0900

    etc/emacs: don't display line numbers for dired

--- etc/emacs/packages/editor-file.el
+++ etc/emacs/packages/editor-file.el
@@ -8,13 +8,23 @@
 
 
 (use-package dired-sidebar
+  :preface
+  (eval-when-compile
+    (declare-function gemacs--dired-sidebar-setup nil))
+
   :general
   (leader
     "TT" #'dired-sidebar-toggle-sidebar
     "Tt" #'dired-sidebar-toggle-with-current-directory)
 
   :config
-  (setq dired-sidebar-theme 'ascii))
+  (setq dired-sidebar-theme 'ascii)
+
+  (defun gemacs--dired-sidebar-setup ()
+    (display-line-numbers-mode -1)
+    (display-fill-column-indicator-mode -1))
+
+  (add-hook 'dired-sidebar-mode-hook #'gemacs--dired-sidebar-setup))
 
 
 (use-package osx-trash
