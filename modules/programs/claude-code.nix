fc22181623f6d30c1c6bf4a042af2dd49a7be4e3
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-13 10:51:32 +0900

    programs/claude-code: back to unstable, enable mcp

--- modules/programs/claude-code.nix
+++ modules/programs/claude-code.nix
@@ -2,7 +2,15 @@
 
 {
   home.packages = [
-    pkgs.local.claude-code
+    (pkgs.unstable.claude-code.overrideDerivation (attrs: {
+      # MCP typically requires npx/uvx/etc.; let's make sure that
+      # nodejs/uv is always available to claude
+      postInstall = attrs.postInstall + ''
+        wrapProgram $out/bin/claude \
+          --prefix PATH : ${pkgs.nodejs}/bin \
+          --prefix PATH : ${pkgs.uv}/bin
+      '';
+    }))
   ];
 
   programs.git = {
