1091181061d53bfd2e92be486b924ec24e304639
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-19 11:32:41 +0900

    programs/aider-chat: configure model

--- modules/programs/aider-chat.nix
+++ modules/programs/aider-chat.nix
@@ -11,12 +11,26 @@
     ];
   };
 
-  home.file.".aider.conf.yml" = {
-    text = ''
-      dark-mode: true
-      git: false
-      auto-commits: false
-      dirty-commits: false
-    '';
+  home.file = {
+    ".aider.conf.yml" = {
+      source = (pkgs.formats.yaml { }).generate "aider.conf.yml" {
+        dark-mode = true;
+        git = false;
+        auto-commits = false;
+        dirty-commits = false;
+      };
+    };
+
+    ".aider.model.settings.yml" = {
+      source = (pkgs.formats.yaml { }).generate "aider.model.settings.yml" [
+        {
+          name = "openrouter/google/gemini-2.5-pro-preview";
+          edit_format = "diff-fenced";
+          weak_model_name = "openrouter/openai/gpt-4o-mini";
+          use_repo_map = true;
+          overeager = true;
+        }
+      ];
+    };
   };
 }
