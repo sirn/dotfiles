c986d59ae645326f0585252a91b1a2b504ac53ee
Sirn Thanabulpong <sirn@ogsite.net>
2025-05-23 12:02:33 +0900

    programs/aider-chat: add claude-sonnet-4 model

--- modules/programs/aider-chat.nix
+++ modules/programs/aider-chat.nix
@@ -24,9 +24,23 @@
     ".aider.model.settings.yml" = {
       source = (pkgs.formats.yaml { }).generate "aider.model.settings.yml" [
         {
+          name = "anthropic/claude-sonnet-4-20250514";
+          edit_format = "diff";
+          weak-model-name = "anthropic/claude-3-5-haiku-20241022";
+          use_repo_map = true;
+          overeager = true;
+        }
+        {
           name = "openrouter/google/gemini-2.5-pro-preview";
           edit_format = "diff-fenced";
-          weak_model_name = "openrouter/openai/gpt-4o-mini";
+          weak_model_name = "openrouter/google/gpt-4o-mini";
+          use_repo_map = true;
+          overeager = true;
+        }
+        {
+          name = "openrouter/anthropic/claude-sonnet-4";
+          edit_format = "diff";
+          weak_model_name = "openrouter/anthropic/claude-3-5-haiku";
           use_repo_map = true;
           overeager = true;
         }
