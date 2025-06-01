4e77051c2928437f09e843c09017a9405d337a40
Sirn Thanabulpong <sirn@ogsite.net>
2025-06-01 18:45:32 +0900

    programs/firefox: fix insanity that is firefox pdf download

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -19,23 +19,24 @@ in
       main = {
         extraConfig = ''
           // Behaviors & UI
+          user_pref("browser.compactmode.show", true);
           user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+          user_pref("browser.download.open_pdf_attachments_inline", true);
           user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
           user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
           user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
           user_pref("browser.sessionstore.warnOnQuit", true);
           user_pref("browser.startup.page", 3);
-          user_pref("browser.compactmode.show", true);
           user_pref("browser.urlbar.oneOffSearches", true);
           user_pref("browser.urlbar.suggest.searches", true);
           user_pref("browser.urlbar.userMadeSearchSuggestionsChoice", true);
           user_pref("extensions.pocket.enabled", false);
+          user_pref("font.cjk_pref_fallback_order", "ja,zh-cn,zh-hk,zh-tw,ko");
           user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
           user_pref("signon.prefillForms", false);
           user_pref("signon.rememberSignons", false);
-          user_pref("font.cjk_pref_fallback_order", "ja,zh-cn,zh-hk,zh-tw,ko");
-          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
           user_pref("svg.context-properties.content.enabled", true);
+          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
           // Privacy protection
           user_pref("browser.contentblocking.category", "strict");
