c7c721c08799a97fcd6d31239d3b2df11a4c6e15
Sirn Thanabulpong <sirn@ogsite.net>
2025-06-08 21:42:58 +0900

    home/fonts: decided that I can't stand default fonts after all

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -2,9 +2,39 @@
 
 let
   dotfilesDir = "${config.home.homeDirectory}/.dotfiles";
+
+  aliasFont = family: font: ''
+    <alias>
+      <family>${family}</family>
+      <prefer>
+        <family>${font}</family>
+      </prefer>
+    </alias>
+  '';
+
+  substituteFont = srcFont: dstFont: ''
+    <match target="pattern">
+      <test name="family" compare="eq">
+        <string>${srcFont}</string>
+      </test>
+      <edit name="family" mode="assign">
+        <string>${dstFont}</string>
+      </edit>
+    </match>
+  '';
+
+  editMode = name: type: value: ''
+    <edit mode="assign" name="${name}">
+      <${type}>${value}</${type}>
+    </edit>
+  '';
 in
 {
-  fonts.fontconfig.enable = pkgs.stdenv.isLinux;
+  fonts = {
+    fontconfig = {
+      enable = true;
+    };
+  };
 
   home.packages = with pkgs; [
     fira
@@ -12,13 +42,15 @@ in
     fira-code-symbols
     fira-mono
     hack-font
-    ia-writer-duospace
     ibm-plex
     inter
     iosevka
+    ipaexfont
+    ipafont
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
+    noto-fonts-color-emoji
     noto-fonts-extra
     source-code-pro
     source-han-code-jp
@@ -38,5 +70,51 @@ in
   # https://wiki.archlinux.org/title/Font_configuration
   gtk.gtk4.extraConfig = lib.mkIf pkgs.stdenv.isLinux {
     gtk-hint-font-metrics = true;
+  };
+
+  home.file = lib.mkIf pkgs.stdenv.isLinux {
+    ".config/fontconfig/conf.d/00-default.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          ${aliasFont "sans-serif" "Noto Sans"}
+          ${aliasFont "serif" "Noto Serif"}
+          ${aliasFont "monospace" "Hack"}
+        </fontconfig>
+      '';
+    };
+    ".config/fontconfig/conf.d/50-substitute.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          ${substituteFont "Arial" "sans-serif"}
+          ${substituteFont "Helvetica" "sans-serif"}
+          ${substituteFont "Segoe UI" "sans-serif"}
+          ${substituteFont "Tahoma" "sans-serif"}
+          ${substituteFont "Verdana" "sans-serif"}
+          ${substituteFont "Times New Roman" "serif"}
+          ${substituteFont "Times" "serif"}
+          ${substituteFont "Courier New" "monospace"}
+          ${substituteFont "Menlo" "monospace"}
+        </fontconfig>
+      '';
+    };
+    ".config/fontconfig/conf.d/99-antialias.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          <match target="font">
+            ${editMode "antialias" "bool" "true"}
+            ${editMode "hinting" "bool" "true"}
+            ${editMode "hintstyle" "const" "hintnone"}
+            ${editMode "lcdfilter" "const" "lcddefault"}
+            ${editMode "rgba" "const" "rgb"}
+          </match>
+        </fontconfig>
+      '';
+    };
   };
 }
