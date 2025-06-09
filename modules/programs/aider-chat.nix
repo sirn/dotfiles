403ec503182368582cb3f782e99c7afa20a7c7da
Sirn Thanabulpong <sirn@ogsite.net>
2025-06-09 23:19:04 +0900

    programs/aider-chat: disable git by default

--- modules/programs/aider-chat.nix
+++ modules/programs/aider-chat.nix
@@ -15,6 +15,12 @@
     ".aider.conf.yml" = {
       source = (pkgs.formats.yaml { }).generate "aider.conf.yml" {
         dark-mode = true;
+
+        # Disable Git unless explicitly enabled since Aider will nag
+        # about creating Git otherwise (I'm using jj...)
+        no-git = true;
+
+        # Don't auto commit.
         auto-commits = false;
         dirty-commits = false;
       };
