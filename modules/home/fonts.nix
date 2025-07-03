6a0854efa4be8ea4c3cc4e5b19abd45fd42b7e97
Sirn Thanabulpong <sirn@ogsite.net>
2025-07-03 23:03:39 +0900

    home/fonts: set this, after all

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -3,6 +3,40 @@
 let
   dotfilesDir = "${config.home.homeDirectory}/.dotfiles";
 
+  aliasFont = family: font: ''
+    <alias binding="strong">
+      <family>${family}</family>
+      <prefer>
+        <family>${font}</family>
+      </prefer>
+    </alias>
+  '';
+
+  aliasFontLang = lang: family: font: ''
+    <match target="pattern">
+      <test name="lang">
+        <string>${lang}</string>
+      </test>
+      <test name="family">
+        <string>${family}</string>
+      </test>
+      <edit name="family" mode="prepend" binding="strong">
+        <string>${font}</string>
+      </edit>
+    </match>
+  '';
+
+  substituteFont = srcFont: dstFont: ''
+    <match target="pattern">
+      <test name="family" compare="eq">
+        <string>${srcFont}</string>
+      </test>
+      <edit name="family" mode="assign" binding="strong">
+        <string>${dstFont}</string>
+      </edit>
+    </match>
+  '';
+
   editMode = name: type: value: ''
     <edit mode="assign" name="${name}">
       <${type}>${value}</${type}>
@@ -53,6 +87,48 @@ in
   };
 
   home.file = lib.mkIf pkgs.stdenv.isLinux {
+    ".config/fontconfig/conf.d/90-substitute.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          ${substituteFont "Arial" "sans-serif"}
+          ${substituteFont "Courier New" "monospace"}
+          ${substituteFont "Helvetica Neue" "sans-serif"}
+          ${substituteFont "Helvetica" "sans-serif"}
+          ${substituteFont "Menlo" "monospace"}
+          ${substituteFont "Segoe UI" "sans-serif"}
+          ${substituteFont "Tahoma" "sans-serif"}
+          ${substituteFont "Times New Roman" "serif"}
+          ${substituteFont "Times" "serif"}
+          ${substituteFont "Verdana" "sans-serif"}
+        </fontconfig>
+      '';
+    };
+    ".config/fontconfig/conf.d/97-lang.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          ${aliasFontLang "ja" "serif" "Noto Serif CJK JP"}
+          ${aliasFontLang "ja" "sans-serif" "Noto Sans CJK JP"}
+          ${aliasFontLang "th" "serif" "Loma"}
+          ${aliasFontLang "th" "sans-serif" "Waree"}
+          ${aliasFontLang "th" "monospace" "Tlwg Typewriter"}
+        </fontconfig>
+      '';
+    };
+    ".config/fontconfig/conf.d/98-default.conf" = {
+      text = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          ${aliasFont "sans-serif" "Noto Sans"}
+          ${aliasFont "serif" "Noto Serif"}
+          ${aliasFont "monospace" "Hack"}
+        </fontconfig>
+      '';
+    };
     ".config/fontconfig/conf.d/99-antialias.conf" = {
       text = ''
         <?xml version='1.0'?>
