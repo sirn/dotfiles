a0dffa4725c22897c465a12e11e5d1909ce270f2
Sirn Thanabulpong <sirn@ogsite.net>
2025-07-19 15:35:04 +0900

    programs/podman: use podman machine on darwin

--- modules/programs/podman.nix
+++ modules/programs/podman.nix
@@ -3,8 +3,6 @@
 {
   home.packages = with pkgs; [
     skopeo
-  ] ++
-  (if pkgs.stdenv.isLinux then [
     podman
     podman-compose
 
@@ -17,18 +15,7 @@
       #!${pkgs.bash}/bin/bash
       exec ${podman-compose}/bin/podman-compose "$@"
     '')
-  ] else [ ]) ++
-  (if pkgs.stdenv.isDarwin then [
-    (pkgs.writeScriptBin "podman" ''
-      #!${pkgs.bash}/bin/bash
-      exec /usr/local/bin/docker "$@"
-    '')
-
-    (pkgs.writeScriptBin "podman-compose" ''
-      #!${pkgs.bash}/bin/bash
-      exec /usr/local/bin/docker-compose "$@"
-    '')
-  ] else [ ]);
+  ];
 
   # On non-NixOS, these paths must be defined separately.
   xdg.configFile = lib.mkIf (!pkgs.stdenv.isDarwin && !config.machine.isNixOS) {
