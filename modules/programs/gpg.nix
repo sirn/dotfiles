a417e3508d884d0d6b2cb1551d80e56abd3c1e6a
Sirn Thanabulpong <sirn@ogsite.net>
2025-07-31 11:30:08 +0900

    programs/gpg: add fish and zsh hooks for updatestartuptty

--- modules/programs/gpg.nix
+++ modules/programs/gpg.nix
@@ -1,5 +1,8 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.programs.gpg;
+in
 {
   programs.gpg = {
     enable = lib.mkDefault true;
@@ -8,4 +11,18 @@
       default-key = "0x65513F82ACB5F854";
     };
   };
+
+  programs.fish.interactiveShellInit = ''
+    function __gpg_updatestartuptty --on-event fish_preexec
+      ${cfg.package}/bin/gpg-connect-agent --no-autostart updatestartuptty /bye >/dev/null 2>&1
+    end
+  '';
+
+  programs.zsh.initExtra = ''
+    __gpg_updatestartuptty {
+      ${cfg.package}/bin/gpg-connect-agent --no-autostart updatestartuptty /bye >/dev/null 2>&1
+    }
+
+    preexec_functions+=(__gpg_updatestartuptty)
+  '';
 }
