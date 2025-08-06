63070c307ea11b0bc6b685aa1a1119ada712d29f
Sirn Thanabulpong <sirn@ogsite.net>
2025-08-07 01:45:38 +0900

    etc/emacs: use the hack from github thread for eat with-editor

--- etc/emacs/packages/editor-term.el
+++ etc/emacs/packages/editor-term.el
@@ -6,8 +6,8 @@
 
 
 (defun gemacs--term-with-editor-setup ()
-  (unless (string-match-p "\\*claude-code\\[.*\\]\\*" (buffer-name)
-            (with-editor-export-editor))))
+  (unless (string-match-p "\\*claude-code\\[.*\\]\\*" (buffer-name))
+    (with-editor-export-editor)))
 
 
 ;; Builtin
@@ -62,6 +62,33 @@
       (eat)))
 
   :config
+  (add-hook 'eat-mode-hook #'gemacs--term-setup)
+
   ;; Note: with-editor doesn't support eat just yet
   ;; See also https://github.com/magit/with-editor/discussions/128
-  (add-hook 'eat-mode-hook #'gemacs--term-setup))
+  (cl-defun with-editor-export-editor-eat (process &optional (envvar "EDITOR"))
+    "Like `with-editor-export-editor', but for `eat-exec-hook'."
+    (cond
+     ((derived-mode-p 'eat-mode)
+      (if with-editor-emacsclient-executable
+          (let ((with-editor--envvar envvar)
+                (process-environment process-environment))
+            (with-editor--setup)
+            (while (accept-process-output process 0.1))
+            (when-let ((v (getenv envvar)))
+              (eat-term-send-string eat-terminal (format " export %s=%S" envvar v))
+              (eat-self-input 1 'return))
+            (when-let ((v (getenv "EMACS_SERVER_FILE")))
+              (eat-term-send-string eat-terminnal (format " export EMACS_SERVER_FILE=%S" v))
+              (eat-self-input 1 'return))
+            (eat-term-send-string eat-terminal "clear")
+            (eat-self-input 1 'return))
+        (error "Cannot use sleeping editor in this buffer")))
+     (t (error "Cannot export environment variables in this buffer")))
+    (message "Successfully exported %s" envvar))
+
+  (defun gemacs--eat-with-editor-setup (process)
+    (unless (string-match-p "\\*claude-code\\[.*\\]\\*" (buffer-name))
+      (with-editor-export-editor-eat process)))
+
+  (add-hook 'eat-exec-hook #'gemacs--eat-with-editor-setup))
