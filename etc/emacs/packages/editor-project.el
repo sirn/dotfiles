fb560115e5b57e2484ce4935bfe561c7c30ff547
Sirn Thanabulpong <sirn@ogsite.net>
2025-08-19 18:20:50 +0900

    etc/emacs: fix jj project root marker

--- etc/emacs/packages/editor-project.el
+++ etc/emacs/packages/editor-project.el
@@ -26,7 +26,7 @@
     "S" #'gemacs--project-sync)
 
   :custom
-  (project-vc-extra-root-markers '("go.mod"))
+  (project-vc-extra-root-markers '(".jj" "go.mod"))
 
   :preface
   (eval-when-compile
