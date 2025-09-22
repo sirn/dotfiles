d293bab6d14a9477bcb9e846cc2e7c73c8966b0f
Sirn Thanabulpong <sirn@ogsite.net>
2025-09-22 16:35:28 +0900

    machines/polaris: switch to sway/niri

--- modules/machines/polaris.nix
+++ modules/machines/polaris.nix
@@ -1,21 +1,24 @@
 {
   flatpak.enable = true;
+  machine.desktop.preferDark = true;
 
   imports = [
     ../common.nix
     ../common-linux.nix
 
     # profiles
+    ../home/breeze.nix
     ../home/flatpak.nix
     ../home/fonts.nix
-    ../home/plasma.nix
+    ../home/i18n.nix
+    ../home/niri.nix
+    ../home/sway.nix
 
     # programs
     ../programs/1password.nix
     ../programs/ffmpeg.nix
     ../programs/imagemagick.nix
     ../programs/intellij.nix
-    ../programs/krita.nix
     ../programs/mpv.nix
     ../programs/sublime-text.nix
     ../programs/wezterm.nix
--- modules/machines/terra.nix
+++ modules/machines/terra.nix
@@ -1,5 +1,3 @@
-{ pkgs, config, ... }:
-
 {
   flatpak.enable = true;
   machine.desktop.preferDark = true;
