64979c53bd8d58c46248c83d84aa481fc2476c3f
Sirn Thanabulpong <sirn@ogsite.net>
2025-10-09 13:02:40 +0900

    pkgs/mcp-servers/fetch: use pypi

--- pkgs/by-name/mcp-servers/fetch/package.nix
+++ pkgs/by-name/mcp-servers/fetch/package.nix
@@ -4,9 +4,10 @@ pkgs.python3Packages.buildPythonPackage rec {
   pname = "mcp-server-fetch";
   version = "2025.4.7";
 
-  src = pkgs.fetchurl {
-    url = "https://files.pythonhosted.org/packages/02/0f/718dde44fd8b8d2792ae6011a3d2c3080440af48cee5d2ee8b7941b75e8e/mcp_server_fetch-2025.4.7.tar.gz";
-    hash = "sha256-VieePFXLHlBrlYypuyPtRBOUSm8jC8oh4ESu5Rc0/kc=";
+  src = pkgs.fetchPypi {
+    pname = "mcp_server_fetch";
+    inherit version;
+    sha256 = "sha256-VieePFXLHlBrlYypuyPtRBOUSm8jC8oh4ESu5Rc0/kc=";
   };
 
   format = "pyproject";
