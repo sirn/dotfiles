48e29cec6dac3f4358c1b5c30cc63cc07775e205
Sirn Thanabulpong <sirn@ogsite.net>
2025-10-16 11:08:16 +0900

    programs/xdg-portal: use kde portal

--- modules/programs/xdg-portal.nix
+++ modules/programs/xdg-portal.nix
@@ -17,8 +17,9 @@ in
     ".config/xdg-desktop-portal/portals.conf" = {
       text = lib.generators.toINI { } {
         preferred = {
-          default = "gtk";
+          default = "kde";
           "org.freedesktop.impl.portal.Inhibit" = "none";
+          "org.freedesktop.impl.portal.Settings" = "gtk;kde";
         } // (if (swaycfg.enable || niricfg.enable) then {
           "org.freedesktop.impl.portal.Screencast" = "wlr";
           "org.freedesktop.impl.portal.Screenshot" = "wlr";
