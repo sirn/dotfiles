e56618518893ec4101874064f4f064c393d4531a
Sirn Thanabulpong <sirn@ogsite.net>
2025-10-18 00:46:17 +0900

    services/kanshi: simplify settings

--- modules/services/kanshi.nix
+++ modules/services/kanshi.nix
@@ -16,59 +16,33 @@ in
   services.kanshi = {
     enable = true;
 
-    settings =
-      let
-        default = {
-          status = lib.mkDefault "enable";
-          criteria = "*";
-          mode = "3840x2160";
-          position = "0,0";
-          scale = 2.0;
+    settings = [
+      {
+        profile = {
+          name = "only_3225qf";
+          outputs = [
+            {
+              criteria = "Dell Inc. AW3225QF 13T4YZ3";
+              mode = "3840x2160";
+              position = "0,0";
+              scale = 1.5;
+            }
+          ];
         };
-
-        apple_pro_display_xdr = default // {
-          criteria = "Apple Computer Inc ProDisplayXDR 0x00001F07";
-          mode = "6016x3384";
-          scale = 2.0;
+      }
+      {
+        profile = {
+          name = "only_pa148";
+          outputs = [
+            {
+              criteria = "ASUSTek COMPUTER INC ASUS PA148 N9LMTF061468";
+              mode = "1920x1080";
+              scale = 1.5;
+            }
+          ];
         };
-
-        dell_aw3225qf = default // {
-          criteria = "Dell Inc. AW3225QF 13T4YZ3";
-          scale = 1.5;
-        };
-
-        asus_pa148 = {
-          criteria = "ASUSTek COMPUTER INC ASUS PA148 N9LMTF061468";
-          mode = "1920x1080";
-          scale = 1.5;
-        };
-      in
-      [
-        {
-          profile = {
-            name = "only_3225qf";
-            outputs = [
-              dell_aw3225qf
-            ];
-          };
-        }
-        {
-          profile = {
-            name = "only_xdr";
-            outputs = [
-              apple_pro_display_xdr
-            ];
-          };
-        }
-        {
-          profile = {
-            name = "only_pa148";
-            outputs = [
-              asus_pa148
-            ];
-          };
-        }
-      ];
+      }
+    ];
   };
 
   wayland.windowManager.sway = lib.mkIf swaycfg.enable {
