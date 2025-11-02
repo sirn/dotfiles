3b7793c60e3a6af94344c9746dc88403d4881d68
Sirn Thanabulpong <sirn@ogsite.net>
2025-11-02 17:34:57 +0900

    programs/ssh: drop display match
    libssh doesn't like this, apparently

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -20,16 +20,6 @@
           "PreferredAuthentications" = "publickey";
         };
       };
-
-      # This is used for when 1Password agent or Bitwarden agent is enabled;
-      # we want to fallback to default agent when we're not running under
-      # X or Wayland.
-      "console" = lib.mkIf pkgs.stdenv.isLinux (lib.hm.dag.entryBefore [ "*" ] {
-        match = "exec \"test -z \\\"$DISPLAY\\\" && test -z \\\"$WAYLAND_DISPLAY\\\"\"";
-        extraOptions = {
-          "IdentityAgent" = "$SSH_AUTH_SOCK";
-        };
-      });
     };
 
     userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
