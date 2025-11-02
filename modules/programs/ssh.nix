287220386c7940e2b872d5353b6c55878610fde8
Sirn Thanabulpong <sirn@ogsite.net>
2025-11-02 14:58:59 +0900

    programs/ssh: remove silly config

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -12,63 +12,25 @@
     serverAliveCountMax = 3;
     serverAliveInterval = 30;
 
-    matchBlocks =
-      let
-        insertBefore = lib.hm.dag.entryBefore [ "*" "console" ];
-      in
-      {
-        "*" = {
-          extraOptions = {
-            "CheckHostIP" = "yes";
-            "StrictHostKeyChecking" = "accept-new";
-            "PreferredAuthentications" = "publickey";
-          };
-        };
-
-        # This is used for when 1Password agent or Bitwarden agent is enabled;
-        # we want to fallback to default agent when we're not running under
-        # X or Wayland.
-        "console" = lib.mkIf pkgs.stdenv.isLinux (lib.hm.dag.entryBefore [ "*" ] {
-          match = "exec \"test -z \\\"$DISPLAY\\\" && test -z \\\"$WAYLAND_DISPLAY\\\"\"";
-          extraOptions = {
-            "IdentityAgent" = "$SSH_AUTH_SOCK";
-          };
-        });
-
-        "bitbucket.org" = insertBefore {
-          user = "git";
-          extraOptions = {
-            "CheckHostIP" = "no";
-          };
-        };
-
-        "github.com" = insertBefore {
-          user = "git";
-          extraOptions = {
-            "CheckHostIP" = "no";
-          };
-        };
-
-        "gitlab.com" = insertBefore {
-          user = "git";
-          extraOptions = {
-            "CheckHostIP" = "no";
-          };
-        };
-
-        "git.sr.ht" = insertBefore {
-          user = "git";
-          extraOptions = {
-            "CheckHostIP" = "no";
-          };
-        };
-
-        "lish-*.linode.com" = insertBefore {
-          extraOptions = {
-            "CheckHostIP" = "no";
-          };
+    matchBlocks = {
+      "*" = {
+        extraOptions = {
+          "CheckHostIP" = "yes";
+          "StrictHostKeyChecking" = "accept-new";
+          "PreferredAuthentications" = "publickey";
         };
       };
+
+      # This is used for when 1Password agent or Bitwarden agent is enabled;
+      # we want to fallback to default agent when we're not running under
+      # X or Wayland.
+      "console" = lib.mkIf pkgs.stdenv.isLinux (lib.hm.dag.entryBefore [ "*" ] {
+        match = "exec \"test -z \\\"$DISPLAY\\\" && test -z \\\"$WAYLAND_DISPLAY\\\"\"";
+        extraOptions = {
+          "IdentityAgent" = "$SSH_AUTH_SOCK";
+        };
+      });
+    };
 
     userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
 
