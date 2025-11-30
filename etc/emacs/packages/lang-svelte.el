308a8ffc5a67cab0cd5b36414886c8b5e28be192
Sirn Thanabulpong <sirn@ogsite.net>
2025-11-30 14:51:14 +0900

    etc/emacs: svelte auto format requires prettier, disable for now

--- etc/emacs/packages/lang-svelte.el
+++ etc/emacs/packages/lang-svelte.el
@@ -6,12 +6,8 @@
     (declare-function gemacs--svelte-auto-format nil))
 
   :init
-  (defun gemacs--svelte-auto-format ()
-    (add-hook 'before-save-hook #'gemacs--eglot-format-buffer -10 t))
-
   (add-hook 'svelte-mode-hook #'eglot-ensure)
   (add-hook 'svelte-mode-hook #'flycheck-mode)
-  (add-hook 'svelte-mode-hook #'gemacs--svelte-auto-format)
 
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs '(svelte-mode . ("svelteserver" "--stdio")))))
