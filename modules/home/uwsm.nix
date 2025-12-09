f938beefe5d0a875465f3583e4f92482eec4b31b
Sirn Thanabulpong <sirn@ogsite.net>
2025-12-09 14:58:03 +0900

    home/uwsm: override slice

--- modules/home/uwsm.nix
+++ modules/home/uwsm.nix
@@ -3,9 +3,14 @@
 let
   swaycfg = config.wayland.windowManager.sway;
 
-  niricfg = config.programs.niri;
-
   fuzzelcfg = config.programs.fuzzel;
+
+  # Slice assignment based on:
+  # https://github.com/Vladimir-csp/uwsm/
+
+  appGraphicalSlice = "app-graphical.slice";
+
+  backgroundGraphicalSlice = "background-graphical.slice";
 
   uwsmFinalize = pkgs.writeShellScript "uwsm-finalize" ''
     if command -v uwsm >/dev/null; then
@@ -23,6 +28,44 @@ in
   programs.fuzzel.settings = lib.mkIf fuzzelcfg.enable {
     main = {
       launch-prefix = "uwsm app --";
+    };
+  };
+
+  systemd.user.services = {
+    waybar.Service = lib.mkIf config.programs.waybar.enable {
+      Slice = appGraphicalSlice;
+    };
+
+    emacs.Service = lib.mkIf (config.systemd.user.services ? emacs) {
+      Slice = appGraphicalSlice;
+    };
+
+    languagetool.Service = lib.mkIf (config.systemd.user.services ? languagetool) {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    swww.Service = lib.mkIf config.services.swww.enable {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    "swww-wallpaper".Service = lib.mkIf config.services.swww.enable {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    wlsunset.Service = lib.mkIf config.services.wlsunset.enable {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    kanshi.Service = lib.mkIf config.services.kanshi.enable {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    swayidle.Service = lib.mkIf config.services.swayidle.enable {
+      Slice = backgroundGraphicalSlice;
+    };
+
+    sway-audio-idle-inhibit.Service = lib.mkIf (config.systemd.user.services ? sway-audio-idle-inhibit) {
+      Slice = backgroundGraphicalSlice;
     };
   };
 }
