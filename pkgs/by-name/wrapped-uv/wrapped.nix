c8f0ac62a0a551af445f16846828f48f822314f6
Sirn Thanabulpong <sirn@ogsite.net>
2025-12-30 15:17:39 +0900

    pkgs/wrapped-uv: make final derivation somewhat consistent between linux/darwin

--- pkgs/by-name/wrapped-uv/wrapped.nix
+++ pkgs/by-name/wrapped-uv/wrapped.nix
@@ -13,26 +13,28 @@ let
       stdenv.cc.cc
       zlib
     ];
+
+    meta.mainProgram = "uv";
   };
 
-  wrappedFhsUv = pkgs.stdenv.mkDerivation {
-    pname = "wrapped-uv";
-    src = ./.;
-    version = pkgs.uv.version;
-
-    nativeBuildInputs = [
-      pkgs.makeWrapper
-    ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-
-      makeWrapper ${fhsUv}/bin/${fhsUv.name} $out/bin/uv
-      makeWrapper ${fhsUv}/bin/${fhsUv.name} $out/bin/uvx --add-flags "tool uvx"
-    '';
-  };
+  actualUv =
+    if pkgs.stdenv.isLinux
+    then fhsUv
+    else pkgs.uv;
 in
+pkgs.stdenv.mkDerivation {
+  pname = "wrapped-uv";
+  src = ./.;
+  version = pkgs.uv.version;
 
-if pkgs.stdenv.isLinux
-then wrappedFhsUv
-else pkgs.uv
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+
+    makeWrapper ${actualUv}/bin/${actualUv.meta.mainProgram} $out/bin/uv
+    makeWrapper ${actualUv}/bin/${actualUv.meta.mainProgram} $out/bin/uvx --add-flags "tool uvx"
+  '';
+}
