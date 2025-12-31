327de720b2a403178b651e6d2138ac4454e4e160
Sirn Thanabulpong <sirn@ogsite.net>
2025-12-31 16:26:08 +0900

    services/podman: no longer link systemd units from pkgs

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -17,13 +17,6 @@ in
     else [ ]
   );
 
-  # On Linux, also enable socket activation
-  xdg.configFile = lib.mkIf cfg.enable {
-    "systemd/user/podman.socket".source = "${cfg.package}/share/systemd/user/podman.socket";
-    "systemd/user/podman.service".source = "${cfg.package}/share/systemd/user/podman.service";
-    "systemd/user/sockets.target.wants/podman.socket".source = "${cfg.package}/share/systemd/user/podman.socket";
-  };
-
   # On Linux, also set DOCKER_HOST to use Podman socket
   home.sessionVariables = lib.mkIf cfg.enable {
     "DOCKER_HOST" = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
