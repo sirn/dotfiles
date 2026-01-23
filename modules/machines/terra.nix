43d67fd5a3077c5583da6083c4c8c1e961748bc3
Sirn Thanabulpong <sirn@ogsite.net>
2026-01-24 01:09:49 +0900

    machines/terra: terra for desktop use is no longer a thing

--- modules/machines/terra.nix
+++ modules/machines/terra.nix
@@ -1,44 +1,6 @@
 {
-  flatpak.enable = true;
-
   imports = [
     ../common.nix
     ../common-linux.nix
-
-    # profiles
-    ../home/breeze.nix
-    ../home/breeze-dark.nix
-    ../home/flatpak.nix
-    ../home/fonts.nix
-    ../home/i18n.nix
-    ../home/niri.nix
-    ../home/sway.nix
-    ../home/uwsm.nix
-
-    # programs
-    ../programs/1password.nix
-    ../programs/ffmpeg.nix
-    ../programs/firefox.nix
-    ../programs/imagemagick.nix
-    ../programs/intellij.nix
-    ../programs/mpv.nix
-    ../programs/sublime-text.nix
-    ../programs/virt-manager.nix
-    ../programs/yt-dlp.nix
-  ];
-
-  services.kanshi.settings = [
-    {
-      profile = {
-        name = "aw3225qf_ipmi";
-        outputs = [
-          { criteria = "$aw3225qf"; }
-          {
-            criteria = "Unknown Unknown Unknown";
-            status = "disable";
-          }
-        ];
-      };
-    }
   ];
 }
