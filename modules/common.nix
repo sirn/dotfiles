ec44d6b992aee7868841eb144e162d95debe225d
Sirn Thanabulpong <sirn@ogsite.net>
2026-02-05 00:18:06 +0900

    common: the day has come where I drop mosh
    since most of mosh usage are either already persisted (home network)
    or already tunnelled via WireGuard (which adds roaming), there doesn't
    seem to be any benefit of using mosh over ssh anymore.
    
    that, and osc52 doesn't work with tmux+mosh for some reason :(

--- modules/common.nix
+++ modules/common.nix
@@ -28,7 +28,6 @@
     ./programs/jq.nix
     ./programs/jujutsu.nix
     ./programs/lstr.nix
-    ./programs/mosh.nix
     ./programs/nix-index.nix
     ./programs/qalculate.nix
     ./programs/repoman.nix
