ba02084576ce92830b2159519a203a3e2d43bb1c
Sirn Thanabulpong <sirn@ogsite.net>
2026-02-21 18:00:20 +0900

    home/shell: fix TERMINFO_DIRS on Darwin

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -6,6 +6,8 @@
 
     # Unless this is set in .profile, Go will loiter go/ in home directory.
     GOPATH = "${config.home.homeDirectory}/Dev/go/gopath:${config.home.homeDirectory}/Dev";
+  } // lib.optionalAttrs pkgs.stdenv.isDarwin {
+    TERMINFO_DIRS = "${config.home.homeDirectory}/.nix-profile/share/terminfo";
   };
 
   home.sessionVariablesExtra =
