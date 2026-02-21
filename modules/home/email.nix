fdb025436a5bfd76bb95dc9c1a455bfa1008feeb
Sirn Thanabulpong <sirn@ogsite.net>
2026-02-21 19:16:36 +0900

    home/email: un-setup email

--- modules/home/email.nix
+++ modules/home/email.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   accounts.email.maildirBasePath = "${config.home.homeDirectory}/Mail";
@@ -9,62 +9,6 @@
       address = "sirn@ogsite.net";
       userName = "sirn@fastmail.fm";
       realName = "Sirn Thanabulpong";
-      passwordCommand = "${pkgs.pass}/bin/pass mail/sirn@fastmail.fm";
-
-      imap = {
-        host = "imap.fastmail.com";
-        tls = {
-          enable = true;
-          useStartTls = false;
-        };
-      };
-
-      smtp = {
-        host = "smtp.fastmail.com";
-        port = 465;
-        tls = {
-          enable = true;
-          useStartTls = false;
-        };
-      };
-
-      folders = {
-        drafts = "Drafts";
-        inbox = "INBOX";
-        sent = "Sent Items";
-        trash = "Trash";
-      };
-
-      maildir = {
-        path = "Ogsite";
-      };
-
-      notmuch = {
-        enable = true;
-      };
-
-      mbsync = {
-        enable = true;
-        create = "both";
-        expunge = "both";
-        flatten = ".";
-        patterns = [
-          "INBOX"
-          "Archive"
-          "Drafts"
-          "Sent Items"
-          "Junk Mail"
-          "Trash"
-          "Feed"
-          "Lists"
-          "Messages"
-          "Noreply"
-        ];
-      };
-
-      msmtp = {
-        enable = true;
-      };
     };
   };
 }
