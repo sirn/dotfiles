72d109dd233c62c034fa37a6d4046758d597d3f8
Sirn Thanabulpong <sirn@ogsite.net>
2026-03-06 18:51:22 +0900

    programs/repoman: update to 0.3.1

--- pkgs/by-name/repoman/package.nix
+++ pkgs/by-name/repoman/package.nix
@@ -2,12 +2,12 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "repoman";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchgit {
     url = "https://git.sr.ht/~sirn/repoman";
     rev = "refs/tags/v${version}";
-    hash = "sha256-OoAMmrjZe6uMsgl9JKNMQnzSoCVz3BAgD6j8jZGbtT0=";
+    hash = "sha256-50GEPRxy5jKxguLvYThji0ZrX9JFCuHtdKGvQF5dp9I=";
   };
 
   cargoHash = "sha256-JkvX5Nu/neBDKN464U4QE9+0e28ksnqg9k7LthzCeMM=";
