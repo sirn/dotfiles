833c15358b7923f0c769afcf72446c96afc01f7c
Sirn Thanabulpong <sirn@ogsite.net>
2026-04-28 00:06:07 +0900

    home-manager/home/home: add .face

--- home-manager/config/home/home.nix
+++ home-manager/config/home/home.nix
@@ -10,6 +10,11 @@
     automatic = true;
   };
 
+  home.file.".face".source = pkgs.fetchurl {
+    url = "https://avatars.githubusercontent.com/u/4061";
+    sha256 = "sha256-Dldkh1eXoxfon0mmNpMYsEo8YSWL29E1+v5V5UyATi8=";
+  };
+
   targets.genericLinux.nixGL.packages =
     if pkgs.stdenv.isLinux && config.targets.genericLinux.enable then pkgs.nixgl else null;
 
