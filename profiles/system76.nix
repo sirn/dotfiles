e46d2ccb117c53abb167049cd38b45b392478a55
Sirn Thanabulpong <sirn@ogsite.net>
2026-05-22 11:49:19 +0900

    profiles/system76: add firefox, sort programs alphabetically

--- profiles/system76.nix
+++ profiles/system76.nix
@@ -121,11 +121,12 @@
 
         # programs
         ../home-manager/config/programs/bitwarden.nix
+        ../home-manager/config/programs/firefox.nix
+        ../home-manager/config/programs/ghostty.nix
         ../home-manager/config/programs/imagemagick.nix
         ../home-manager/config/programs/mcp.nix
         ../home-manager/config/programs/obsidian.nix
         ../home-manager/config/programs/sublime-text.nix
-        ../home-manager/config/programs/ghostty.nix
       ];
 
       home.colors.themeName = "nord";
