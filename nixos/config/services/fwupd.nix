b38fee55845466b603fab60a14d5814b68d4ffc5
Sirn Thanabulpong <sirn@ogsite.net>
2026-06-12 17:28:50 +0900

    nixos/fwupd: workaround auth error

--- nixos/config/services/fwupd.nix
+++ nixos/config/services/fwupd.nix
@@ -2,4 +2,17 @@
   services.fwupd = {
     enable = true;
   };
+
+  # Grant fwupd-refresh system user access to metadata refresh without polkit
+  # auth (headless service user has no seat).
+  # TODO: Remove once nixpkgs includes PR #526476.
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if ((action.id == "org.freedesktop.fwupd.get-remotes" ||
+           action.id == "org.freedesktop.fwupd.refresh-remote") &&
+          subject.user == "fwupd-refresh") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
