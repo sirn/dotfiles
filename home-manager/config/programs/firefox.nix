70d445cacbfb0c85dcd7d695e0ae7df03bea5563
Sirn Thanabulpong <sirn@ogsite.net>
2026-07-04 01:44:43 +0900

    hm/firefox: no longer installing browsers via flatpak

--- home-manager/config/programs/firefox.nix
+++ home-manager/config/programs/firefox.nix
@@ -7,97 +7,14 @@
 
 let
   firefoxProfiles = config.programs.firefox.profiles;
-  firefoxExec =
-    if pkgs.stdenv.isDarwin then
-      "/Applications/Firefox.app/Contents/MacOS/firefox"
-    else if config.flatpak.enable then
-      "flatpak run org.mozilla.firefox"
-    else
-      lib.getExe config.programs.firefox.finalPackage;
-
-  mkFirefoxProfileApp =
-    name:
-    let
-      bundleId = "org.nix-community.home.firefox.${name}";
-      appName = "Firefox (${name})";
-      execName = "firefox-${name}";
-    in
-    pkgs.runCommand "firefox-${name}-app"
-      {
-        meta = {
-          platforms = lib.platforms.darwin;
-        };
-      }
-      ''
-        appDir="$out/Applications/${appName}.app"
-        mkdir -p "$appDir/Contents/MacOS"
-        mkdir -p "$appDir/Contents/Resources"
-
-        cat > "$appDir/Contents/MacOS/${execName}" << 'EOF'
-        #!/bin/bash
-        exec open -n -a /Applications/Firefox.app --args -P "${name}" -no-remote
-        EOF
-        chmod +x "$appDir/Contents/MacOS/${execName}"
-
-        if [ -f "/Applications/Firefox.app/Contents/Resources/firefox.icns" ]; then
-          cp "/Applications/Firefox.app/Contents/Resources/firefox.icns" "$appDir/Contents/Resources/"
-        fi
-
-        cat > "$appDir/Contents/Info.plist" << 'PLISTEOF'
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
-          <key>CFBundleInfoDictionaryVersion</key>
-          <string>6.0</string>
-          <key>CFBundleDevelopmentRegion</key>
-          <string>en</string>
-          <key>CFBundlePackageType</key>
-          <string>APPL</string>
-          <key>CFBundleIdentifier</key>
-          <string>${bundleId}</string>
-          <key>CFBundleExecutable</key>
-          <string>${execName}</string>
-          <key>CFBundleName</key>
-          <string>${appName}</string>
-          <key>CFBundleDisplayName</key>
-          <string>${appName}</string>
-          <key>CFBundleVersion</key>
-          <string>1.0</string>
-          <key>CFBundleShortVersionString</key>
-          <string>1.0</string>
-          <key>CFBundleIconFile</key>
-          <string>firefox</string>
-          <key>LSUIElement</key>
-          <false/>
-          <key>NSHighResolutionCapable</key>
-          <true/>
-          <key>NSSupportsAutomaticGraphicsSwitching</key>
-          <true/>
-        </dict>
-        </plist>
-        PLISTEOF
-      '';
 in
 {
   programs.firefox = {
     enable = true;
 
-    # If NixGL is configured (i.e. non-NixOS), wrap with NixGL
-    # so OpenGL/Vulkan libraries are available. On Darwin and
-    # when we're using Firefox from Flatpak, only configure Firefox.
-    package = lib.mkDefault (
-      if pkgs.stdenv.isLinux && !config.flatpak.enable then config.lib.nixGL.wrap pkgs.firefox else null
-    );
-
-    configPath = lib.mkDefault (
-      if pkgs.stdenv.isDarwin then
-        "Library/Application Support/Firefox"
-      else if config.flatpak.enable then
-        ".var/app/org.mozilla.firefox/.mozilla/firefox"
-      else
-        ".mozilla/firefox"
-    );
+    # If NixGL is configured (i.e. non-NixOS), wrap with NixGL so OpenGL/Vulkan libraries are available.
+    # On macOS, user should install Firefox by themselves.
+    package = lib.mkDefault (if pkgs.stdenv.isLinux then config.lib.nixGL.wrap pkgs.firefox else null);
 
     # By default, this is set to 2, which fails on non-NixOS Firefox
     # https://github.com/nix-community/home-manager/issues/6170
@@ -149,33 +66,103 @@ in
   };
 
   # Linux: XDG desktop entries for non-default Firefox profiles
-  xdg.desktopEntries = lib.mkIf (config.programs.firefox.enable && pkgs.stdenv.isLinux) (
-    lib.mapAttrs' (name: profile: {
-      name = "firefox-${name}";
-      value = {
-        name = "Firefox (${name})";
-        genericName = "Web Browser";
-        exec = "${firefoxExec} -P ${name} %U";
-        icon = "org.mozilla.firefox";
-        terminal = false;
-        categories = [
-          "Network"
-          "WebBrowser"
-        ];
-        mimeType = [
-          "text/html"
-          "text/xml"
-          "application/xhtml+xml"
-        ];
-        startupNotify = true;
-      };
-    }) (lib.filterAttrs (name: _: name != "main") firefoxProfiles)
-  );
+  xdg.desktopEntries =
+    let
+      firefoxExec = lib.getExe config.programs.firefox.finalPackage;
+    in
+    lib.mkIf (config.programs.firefox.enable && pkgs.stdenv.isLinux) (
+      lib.mapAttrs' (name: profile: {
+        name = "firefox-${name}";
+        value = {
+          name = "Firefox (${name})";
+          genericName = "Web Browser";
+          exec = "${firefoxExec} -P ${name} %U";
+          icon = "org.mozilla.firefox";
+          terminal = false;
+          categories = [
+            "Network"
+            "WebBrowser"
+          ];
+          mimeType = [
+            "text/html"
+            "text/xml"
+            "application/xhtml+xml"
+          ];
+          startupNotify = true;
+        };
+      }) (lib.filterAttrs (name: _: name != "main") firefoxProfiles)
+    );
 
   # macOS: Application bundles for non-default Firefox profiles
-  home.packages = lib.mkIf (config.programs.firefox.enable && pkgs.stdenv.isDarwin) (
-    map (name: mkFirefoxProfileApp name) (
-      builtins.filter (name: name != "main") (builtins.attrNames firefoxProfiles)
-    )
-  );
+  home.packages =
+    let
+      mkFirefoxProfileApp =
+        name:
+        let
+          bundleId = "org.nix-community.home.firefox.${name}";
+          appName = "Firefox (${name})";
+          execName = "firefox-${name}";
+        in
+        pkgs.runCommand "firefox-${name}-app"
+          {
+            meta = {
+              platforms = lib.platforms.darwin;
+            };
+          }
+          ''
+            appDir="$out/Applications/${appName}.app"
+            mkdir -p "$appDir/Contents/MacOS"
+            mkdir -p "$appDir/Contents/Resources"
+
+            cat > "$appDir/Contents/MacOS/${execName}" << 'EOF'
+            #!/bin/bash
+            exec open -n -a /Applications/Firefox.app --args -P "${name}" -no-remote
+            EOF
+            chmod +x "$appDir/Contents/MacOS/${execName}"
+
+            if [ -f "/Applications/Firefox.app/Contents/Resources/firefox.icns" ]; then
+              cp "/Applications/Firefox.app/Contents/Resources/firefox.icns" "$appDir/Contents/Resources/"
+            fi
+
+            cat > "$appDir/Contents/Info.plist" << 'PLISTEOF'
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+            <dict>
+              <key>CFBundleInfoDictionaryVersion</key>
+              <string>6.0</string>
+              <key>CFBundleDevelopmentRegion</key>
+              <string>en</string>
+              <key>CFBundlePackageType</key>
+              <string>APPL</string>
+              <key>CFBundleIdentifier</key>
+              <string>${bundleId}</string>
+              <key>CFBundleExecutable</key>
+              <string>${execName}</string>
+              <key>CFBundleName</key>
+              <string>${appName}</string>
+              <key>CFBundleDisplayName</key>
+              <string>${appName}</string>
+              <key>CFBundleVersion</key>
+              <string>1.0</string>
+              <key>CFBundleShortVersionString</key>
+              <string>1.0</string>
+              <key>CFBundleIconFile</key>
+              <string>firefox</string>
+              <key>LSUIElement</key>
+              <false/>
+              <key>NSHighResolutionCapable</key>
+              <true/>
+              <key>NSSupportsAutomaticGraphicsSwitching</key>
+              <true/>
+            </dict>
+            </plist>
+            PLISTEOF
+          '';
+    in
+    lib.mkIf (config.programs.firefox.enable && pkgs.stdenv.isDarwin) (
+      map (name: mkFirefoxProfileApp name) (
+        builtins.filter (name: name != "main") (builtins.attrNames firefoxProfiles)
+      )
+    );
 }
