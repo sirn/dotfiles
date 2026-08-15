46d96dd6708e64bcbe0f013abebd6f918f492a0e
Sirn Thanabulpong <sirn@ogsite.net>
2026-08-16 01:57:59 +0900

    {nixos,hm}/nix: consolidate shared binary cache settings

---
+++ lib/caches.nix
@@ -0,0 +1,12 @@
+{
+  substituters = [
+    "https://cache.numtide.com"
+    "https://noctalia.cachix.org"
+    "https://microvm.cachix.org"
+  ];
+  trusted-public-keys = [
+    "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
+  ];
+}
--- nixos/config/system/nix.nix
+++ nixos/config/system/nix.nix
@@ -1,21 +1,15 @@
 { pkgs, ... }:
-
+let
+  caches = import ../../../lib/caches.nix;
+in
 {
   nix.channel.enable = false;
 
   nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
 
   nix.settings = {
-    substituters = [
-      "https://cache.numtide.com"
-      "https://noctalia.cachix.org"
-      "https://microvm.cachix.org"
-    ];
-    trusted-public-keys = [
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-    ];
+    substituters = caches.substituters;
+    trusted-public-keys = caches.trusted-public-keys;
   };
 
   nix.gc = {
