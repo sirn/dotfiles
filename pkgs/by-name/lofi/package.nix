9c812b9ad3fc6860b5877ecce962321d64393a0a
Sirn Thanabulpong <sirn@ogsite.net>
2026-08-16 16:27:30 +0900

    pkgs/lofi: update to v0.2.2

--- pkgs/by-name/lofi/package.nix
+++ pkgs/by-name/lofi/package.nix
@@ -6,15 +6,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lofi";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchgit {
     url = "https://git.sr.ht/~sirn/lofi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-hJ/X6q0D8hfHh2xCkiq56Ji66trXV6Or5PaTQReX7Do=";
+    hash = "sha256-7SWlNPS5JBiu9/PYABlGagCCZHzJ3R/ZcXjXDKwWLVw=";
   };
 
-  cargoHash = "sha256-h9QhmnAL1dQm9OHTptr4egVkgmZ5ftssM+JcmrUSQXE=";
+  cargoHash = "sha256-Bz1+nLUQh8n3vs81W25hewuO2QTigtj7rGWcLzTG4QY=";
 
   cargoBuildFlags = [
     "-p"
